5a1cbced7c2019af4ae085d4bf01a3db0249e761
shahriar <aerofoil.kite@gmail.com>
2017-05-02 10:47:11 +0600

    Rename status to phase

--- api/database_snapshot_types.go
+++ api/database_snapshot_types.go
@@ -25,21 +25,21 @@ type DatabaseSnapshotSpec struct {
 	SnapshotSpec `json:",inline,omitempty"`
 }
 
-type SnapshotStatus string
+type SnapshotPhase string
 
 const (
 	// used for DatabaseSnapshots that are currently running
-	StatusSnapshotRunning SnapshotStatus = "Running"
+	PhaseSnapshotRunning SnapshotPhase = "Running"
 	// used for DatabaseSnapshots that are Succeeded
-	StatusSnapshotSuccessed SnapshotStatus = "Succeeded"
+	PhaseSnapshotSuccessed SnapshotPhase = "Succeeded"
 	// used for DatabaseSnapshots that are Failed
-	StatusSnapshotFailed SnapshotStatus = "Failed"
+	PhaseSnapshotFailed SnapshotPhase = "Failed"
 )
 
 type DatabaseSnapshotStatus struct {
 	StartTime      *unversioned.Time `json:"startTime,omitempty"`
 	CompletionTime *unversioned.Time `json:"completionTime,omitempty"`
-	Status         SnapshotStatus    `json:"status,omitempty"`
+	Phase          SnapshotPhase     `json:"phase,omitempty"`
 	Reason         string            `json:"reason,omitempty"`
 }
 
