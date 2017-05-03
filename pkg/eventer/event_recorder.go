162c2955c7a54499cfefbcebd2b7c8edd0411b31
shahriar <aerofoil.kite@gmail.com>
2017-05-03 14:21:10 +0600

    Remove EventRecorder wrapper

--- pkg/eventer/event_recorder.go
+++ pkg/eventer/event_recorder.go
@@ -5,7 +5,6 @@ import (
 	kapi "k8s.io/kubernetes/pkg/api"
 	clientset "k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset"
 	"k8s.io/kubernetes/pkg/client/record"
-	"k8s.io/kubernetes/pkg/runtime"
 )
 
 const (
@@ -36,16 +35,7 @@ const (
 	EventReasonSuccessfulInitialize string = "SuccessfulInitialize"
 )
 
-type EventRecorderInterface interface {
-	PushEvent(eventtype, reason, message string, objects ...runtime.Object)
-}
-
-type eventRecorder struct {
-	// Event Recorder
-	record.EventRecorder
-}
-
-func NewEventRecorder(client clientset.Interface, component string) EventRecorderInterface {
+func NewEventRecorder(client clientset.Interface, component string) record.EventRecorder {
 	// Event Broadcaster
 	broadcaster := record.NewBroadcaster()
 	broadcaster.StartEventWatcher(
@@ -55,12 +45,6 @@ func NewEventRecorder(client clientset.Interface, component string) EventRecorde
 			}
 		},
 	)
-	// Event Recorder
-	return &eventRecorder{broadcaster.NewRecorder(kapi.EventSource{Component: component})}
-}
 
-func (e *eventRecorder) PushEvent(eventtype, reason, message string, objects ...runtime.Object) {
-	for _, obj := range objects {
-		e.Event(obj, eventtype, reason, message)
-	}
+	return broadcaster.NewRecorder(kapi.EventSource{Component: component})
 }
