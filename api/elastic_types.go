d7aae13c592157fe6aea0e085709cf024c143eb4
shahriar <aerofoil.kite@gmail.com>
2017-05-16 12:14:02 +0600

    Rename ServiceAccountName in ElasticSpec

--- api/elastic_types.go
+++ api/elastic_types.go
@@ -26,9 +26,9 @@ type ElasticSpec struct {
 	Replicas int32 `json:"replicas,omitempty"`
 	// Storage spec to specify how storage shall be used.
 	Storage *StorageSpec `json:"storage,omitempty"`
-	// ServiceAccountName is the name of the ServiceAccount to use to run the
-	// Prometheus Pods.
-	ServiceAccountName string `json:"serviceAccountName,omitempty"`
+	// GoverningService is the name of Headless Service which is responsible for the network identity
+	// of the Prometheus Pods.
+	GoverningService string `json:"governingService,omitempty"`
 	// NodeSelector is a selector which must be true for the pod to fit on a node
 	// +optional
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
