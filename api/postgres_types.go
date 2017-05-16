52425ea345a59c8260950ccf2b836313c84d70c8
shahriar <aerofoil.kite@gmail.com>
2017-05-16 10:59:40 +0600

    Remove Replicas field from PostgresSpec

--- api/postgres_types.go
+++ api/postgres_types.go
@@ -22,8 +22,6 @@ type Postgres struct {
 type PostgresSpec struct {
 	// Version of Postgres to be deployed.
 	Version string `json:"version,omitempty"`
-	// Number of instances to deploy for a Postgres database.
-	Replicas int32 `json:"replicas,omitempty"`
 	// Storage spec to specify how storage shall be used.
 	Storage *StorageSpec `json:"storage,omitempty"`
 	// ServiceAccountName is the name of the ServiceAccount to use to run the
