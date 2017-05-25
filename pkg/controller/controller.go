69668d89d31c9541245bf03907d2011f0e47a617
Mir Shahriar <aerokite@users.noreply.github.com>
2017-05-25 13:34:53 +0600

    Use kubedb instead of k8sdb (#98)

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -15,7 +15,7 @@ type Controller struct {
 }
 
 const (
-	DatabaseNamePrefix = "k8sdb"
+	DatabaseNamePrefix = "kubedb"
 	LabelDatabaseKind  = "kubedb.com/kind"
 	LabelDatabaseName  = "kubedb.com/name"
 	sleepDuration      = time.Second * 10
