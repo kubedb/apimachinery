726cabfc566d28a22304ae6f5ccaa949165680ab
tamal <tamal@appscode.com>
2017-05-25 02:22:35 -0700

    Handle missing monitor spec for updates

--- pkg/monitor/monitor.go
+++ pkg/monitor/monitor.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Monitor interface {
-	AddMonitor(meta *kapi.ObjectMeta, spec *tapi.MonitorSpec) error
-	UpdateMonitor(meta *kapi.ObjectMeta, old, new *tapi.MonitorSpec) error
-	DeleteMonitor(meta *kapi.ObjectMeta, spec *tapi.MonitorSpec) error
+	AddMonitor(meta kapi.ObjectMeta, spec *tapi.MonitorSpec) error
+	UpdateMonitor(meta kapi.ObjectMeta, old, new *tapi.MonitorSpec) error
+	DeleteMonitor(meta kapi.ObjectMeta, spec *tapi.MonitorSpec) error
 }
