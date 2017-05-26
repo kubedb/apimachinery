9b43a150c94d21c267e1d616b6719dbee8b3d146
sauman <sauman@appscode.com>
2017-05-26 12:43:33 +0600

    added-all

---
+++ pkg/docker/images.go
@@ -0,0 +1,11 @@
+package docker
+
+const (
+	KubedbExporter      = "kubedb/exporter"
+	KubedbPostgres      = "kubedb/postgres"
+	KubedbEsOperator    = "kubedb/es-operator"
+	KubedbElasticsearch = "kubedb/elasticsearch"
+	KubedbElasticdump   = "kubedb/elasticdump"
+	KubedbPgOperator    = "kubedb/pg-operator"
+	KubedbOperator      = "kubedb/operator"
+)
