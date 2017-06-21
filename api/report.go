174bceef0a6233a3025349b24880308c02932ba5
tamal <tamal@appscode.com>
2017-06-21 00:52:14 -0700

    added-all

--- api/report.go
+++ api/report.go
@@ -7,8 +7,8 @@ import (
 type Report struct {
 	metav1.TypeMeta   `json:",inline,omitempty"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Report ReportSummary `json:"report,omitempty"`
-	Status ReportStatus `json:"status,omitempty"`
+	Summary           ReportSummary `json:"summary,omitempty"`
+	Status            ReportStatus  `json:"status,omitempty"`
 }
 
 type ReportSummary struct {
