9b64b4466a546aaa68025f62d421f564d28085e7
tamal <tamal@appscode.com>
2017-07-05 13:25:55 -0700

    Add more helper labels & annotations.

--- api/helpers.go
+++ api/helpers.go
@@ -2,6 +2,15 @@ package api
 
 import "fmt"
 
+const (
+	DatabaseNamePrefix = "kubedb"
+	LabelDatabaseKind  = "kubedb.com/kind"
+	LabelDatabaseName  = "kubedb.com/name"
+
+	PostgresDatabaseVersion = "postgres.kubedb.com/version"
+	ElasticDatabaseVersion  = "elastic.kubedb.com/version"
+)
+
 func (p Postgres) OffshootName() string {
 	return fmt.Sprintf("%v-%v", p.Name, ResourceCodePostgres)
 }
@@ -13,8 +22,26 @@ func (p Postgres) ServiceName() string {
 func (p Postgres) OffshootLabels() map[string]string {
 	return map[string]string{
 		"origin":          "kubedb",
-		"kubedb.com/name": p.Name,
+		LabelDatabaseKind: p.Name,
 	}
+}
+
+func (p Postgres) StatefulSetLabels() map[string]string {
+	labels := make(map[string]string)
+	for key, val := range p.Labels {
+		labels[key] = val
+	}
+	labels[LabelDatabaseKind] = ResourceKindPostgres
+	return labels
+}
+
+func (p Postgres) StatefulSetAnnotations() map[string]string {
+	annotations := make(map[string]string)
+	for key, val := range p.Annotations {
+		annotations[key] = val
+	}
+	annotations[PostgresDatabaseVersion] = string(p.Spec.Version)
+	return annotations
 }
 
 func (e Elastic) OffshootName() string {
@@ -28,6 +55,24 @@ func (e Elastic) ServiceName() string {
 func (e Elastic) OffshootLabels() map[string]string {
 	return map[string]string{
 		"origin":          "kubedb",
-		"kubedb.com/name": e.Name,
+		LabelDatabaseKind: e.Name,
 	}
+}
+
+func (p Elastic) StatefulSetLabels() map[string]string {
+	labels := make(map[string]string)
+	for key, val := range p.Labels {
+		labels[key] = val
+	}
+	labels[LabelDatabaseKind] = ResourceKindElastic
+	return labels
+}
+
+func (p Elastic) StatefulSetAnnotations() map[string]string {
+	annotations := make(map[string]string)
+	for key, val := range p.Annotations {
+		annotations[key] = val
+	}
+	annotations[ElasticDatabaseVersion] = string(p.Spec.Version)
+	return annotations
 }
