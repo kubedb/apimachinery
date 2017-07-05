4f5cf494203f25a448c3443e21e27afa2535f313
tamal <tamal@appscode.com>
2017-07-05 13:04:26 -0700

    added-all

--- api/helpers.go
+++ api/helpers.go
@@ -6,6 +6,10 @@ func (p Postgres) OffshootName() string {
 	return fmt.Sprintf("%v-%v", p.Name, ResourceCodePostgres)
 }
 
+func (p Postgres) ServiceName() string {
+	return p.Name
+}
+
 func (p Postgres) OffshootLabels() map[string]string {
 	return map[string]string{
 		"origin":          "kubedb",
@@ -15,6 +19,10 @@ func (p Postgres) OffshootLabels() map[string]string {
 
 func (e Elastic) OffshootName() string {
 	return fmt.Sprintf("%v-%v", e.Name, ResourceCodeElastic)
+}
+
+func (e Elastic) ServiceName() string {
+	return e.Name
 }
 
 func (e Elastic) OffshootLabels() map[string]string {
