c2a71d26e57d383678e827e3c0049f666dcadc89
shahriar <aerofoil.kite@gmail.com>
2018-01-19 17:14:09 +0600

    fixed

---
+++ pkg/controller/job/controller.go
@@ -0,0 +1,70 @@
+package snapshot
+
+import (
+	"time"
+
+	amc "github.com/kubedb/apimachinery/pkg/controller"
+	"github.com/kubedb/apimachinery/pkg/eventer"
+	batch "k8s.io/api/batch/v1"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/record"
+	"k8s.io/client-go/util/workqueue"
+)
+
+type Job interface {
+	Update(job *batch.Job) error
+	Delete(job *batch.Job) error
+}
+
+type Controller struct {
+	*amc.Controller
+	// Snapshotter interface
+	job Job
+	// ListerWatcher
+	lw *cache.ListWatch
+	// Event Recorder
+	eventRecorder record.EventRecorder
+	// sync time to sync the list.
+	syncPeriod time.Duration
+	// Workqueue
+	indexer  cache.Indexer
+	queue    workqueue.RateLimitingInterface
+	informer cache.Controller
+	//Max number requests for retries
+	maxNumRequests int
+}
+
+// NewController creates a new Controller
+func NewController(
+	controller *amc.Controller,
+	job Job,
+	lw *cache.ListWatch,
+	syncPeriod time.Duration,
+) *Controller {
+
+	// return new DormantDatabase Controller
+	return &Controller{
+		Controller:     controller,
+		job:            job,
+		lw:             lw,
+		eventRecorder:  eventer.NewEventRecorder(controller.Client, "Job Controller"),
+		syncPeriod:     syncPeriod,
+		maxNumRequests: 2,
+	}
+}
+
+func (c *Controller) Run() {
+	// Watch DormantDatabase with provided ListerWatcher
+	c.watchJob()
+}
+
+func (c *Controller) watchJob() {
+
+	c.initWatcher()
+
+	stop := make(chan struct{})
+	defer close(stop)
+
+	c.runWatcher(5, stop)
+	select {}
+}
---
+++ pkg/controller/job/workqueue.go
@@ -0,0 +1,166 @@
+package snapshot
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/appscode/go/log"
+	meta_util "github.com/appscode/kutil/meta"
+	batch "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+func (c *Controller) initWatcher() {
+
+	// create the workqueue
+	c.queue = workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "job")
+
+	// Bind the workqueue to a cache with the help of an informer. This way we make sure that
+	// whenever the cache is updated, the Job key is added to the workqueue.
+	// Note that when we finally process the item from the workqueue, we might see a newer version
+	// of the Job than the version which was responsible for triggering the update.
+	c.indexer, c.informer = cache.NewIndexerInformer(c.lw, &batch.Job{}, c.syncPeriod, cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			// IndexerInformer uses a delta queue, therefore for deletes we have to use this
+			// key function.
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err == nil {
+				c.queue.Add(key)
+			}
+		},
+		UpdateFunc: func(old, new interface{}) {
+			oldObj, ok := old.(*batch.Job)
+			if !ok {
+				log.Errorln("Invalid Job object")
+				return
+			}
+			newObj, ok := new.(*batch.Job)
+			if !ok {
+				log.Errorln("Invalid Job object")
+				return
+			}
+			if !jobStatusEqual(oldObj, newObj) {
+				key, err := cache.MetaNamespaceKeyFunc(new)
+				if err == nil {
+					c.queue.Add(key)
+				}
+			}
+		},
+	}, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc})
+}
+
+func jobStatusEqual(old, new *batch.Job) bool {
+	if !meta_util.Equal(old.Status, new.Status) {
+		diff := meta_util.Diff(old.Status, new.Status)
+		log.Debugf("Job %s/%s has changed. Diff: %s\n", new.Namespace, new.Name, diff)
+		return false
+	}
+	return true
+}
+
+func (c *Controller) runWatcher(threadiness int, stopCh chan struct{}) {
+	defer runtime.HandleCrash()
+
+	// Let the workers stop when we are done
+	defer c.queue.ShutDown()
+	log.Infoln("Starting Job Controller")
+
+	go c.informer.Run(stopCh)
+
+	// Wait for all involved caches to be synced, before processing items from the queue is started
+	if !cache.WaitForCacheSync(stopCh, c.informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("timed out waiting for caches to sync"))
+		return
+	}
+
+	for i := 0; i < threadiness; i++ {
+		go wait.Until(c.runWorker, time.Second, stopCh)
+	}
+
+	<-stopCh
+	log.Infoln("Stopping Job Controller")
+}
+
+func (c *Controller) runWorker() {
+	for c.processNextItem() {
+	}
+}
+
+func (c *Controller) processNextItem() bool {
+	// Wait until there is a new item in the working queue
+	key, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	// Tell the queue that we are done with processing this key. This unblocks the key for other workers
+	// This allows safe parallel processing because two Jobs with the same key are never processed in
+	// parallel.
+	defer c.queue.Done(key)
+
+	// Invoke the method containing the business logic
+	err := c.runJob(key.(string))
+	if err == nil {
+		// Forget about the #AddRateLimited history of the key on every successful synchronization.
+		// This ensures that future processing of updates for this key is not delayed because of
+		// an outdated error history.
+		c.queue.Forget(key)
+		log.Debugf("Finished Processing key: %v\n", key)
+		return true
+	}
+	log.Errorf("Failed to process Job %v. Reason: %s\n", key, err)
+
+	// This Controller retries 5 times if something goes wrong. After that, it stops trying.
+	if c.queue.NumRequeues(key) < c.maxNumRequests {
+		log.Infof("Error syncing crd %v: %v\n", key, err)
+
+		// Re-enqueue the key rate limited. Based on the rate limiter on the
+		// queue and the re-enqueue history, the key will be processed later again.
+		c.queue.AddRateLimited(key)
+		return true
+	}
+
+	c.queue.Forget(key)
+	log.Debugf("Finished Processing key: %v\n", key)
+	// Report to an external entity that, even after several retries, we could not successfully process this key
+	runtime.HandleError(err)
+	log.Infof("Dropping Job %q out of the queue: %v\n", key, err)
+	return true
+}
+
+func (c *Controller) runJob(key string) error {
+	log.Debugf("started processing, key: %v\n", key)
+	obj, exists, err := c.indexer.GetByKey(key)
+	if err != nil {
+		log.Errorf("Fetching object with key %s from store failed with %v\n", key, err)
+		return err
+	}
+
+	if !exists {
+		log.Debugf("Job %s does not exist anymore\n", key)
+	} else {
+		// Note that you also have to check the uid if you have a local controlled resource, which
+		// is dependent on the actual instance, to detect that a Job was recreated with the same name
+		job := obj.(*batch.Job).DeepCopy()
+		if job.DeletionTimestamp != nil {
+			if err := c.job.Delete(job); err != nil {
+				log.Errorln(err)
+				return err
+			}
+		} else {
+			if err := c.job.Update(job); err != nil {
+				log.Errorln(err)
+				return err
+			}
+		}
+	}
+	return nil
+}
