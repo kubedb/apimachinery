3df6fa5a36e78d2d237267ed2d3cb96c448dccae
the-redback <maruf@appscode.com>
2018-01-19 12:24:11 +0600

    Improved Validation of Monitoring Spec

--- pkg/validator/validate.go
+++ pkg/validator/validate.go
@@ -84,14 +84,12 @@ func ValidateMonitorSpec(monitorSpec *mona.AgentSpec) error {
 	}
 
 	if monitorSpec.Agent.Vendor() == mona.VendorPrometheus {
-		if monitorSpec.Agent == mona.AgentCoreOSPrometheus {
-			if monitorSpec.Prometheus == nil {
-				return fmt.Errorf(`invalid 'Agent' in '%v'`, string(specData))
-			}
-		} else if monitorSpec.Agent != mona.AgentPrometheusBuiltin {
-			return fmt.Errorf(`invalid 'Agent' in '%v'`, string(specData))
+		if monitorSpec.Agent == mona.AgentCoreOSPrometheus && monitorSpec.Prometheus != nil {
+			return nil
+		} else if monitorSpec.Agent == mona.AgentPrometheusBuiltin {
+			return nil
 		}
 	}
 
-	return nil
+	return fmt.Errorf(`invalid 'Agent' in '%v'`, string(specData))
 }
