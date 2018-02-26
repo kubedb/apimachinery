b7c96e8a331e38952b22988a833402e9faf95f81
shahriar <aerofoil.kite@gmail.com>
2018-02-26 12:17:03 +0600

    fixed

--- apis/kubedb/v1alpha1/postgres_helpers.go
+++ apis/kubedb/v1alpha1/postgres_helpers.go
@@ -97,8 +97,8 @@ func (p *Postgres) GetMonitoringVendor() string {
 	return ""
 }
 
-func (p Postgres) PrimaryName() string {
-	return fmt.Sprintf("%v-primary", p.Name)
+func (p Postgres) StandbyName() string {
+	return fmt.Sprintf("%v-standby", p.Name)
 }
 
 func (p Postgres) CustomResourceDefinition() *crd_api.CustomResourceDefinition {
