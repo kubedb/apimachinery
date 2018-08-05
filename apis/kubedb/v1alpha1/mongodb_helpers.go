cf203fbf79b345d7c835cfa015b08f1fde69d9b1
Tamal Saha <tamal@appscode.com>
2018-08-04 17:37:06 -0700

    Remove StatefulSetAnnotations for MongoDB
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/kubedb/v1alpha1/mongodb_helpers.go
+++ apis/kubedb/v1alpha1/mongodb_helpers.go
@@ -2,7 +2,6 @@ package v1alpha1
 
 import (
 	"fmt"
-	"strings"
 
 	crdutils "github.com/appscode/kutil/apiextensions/v1beta1"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
@@ -22,16 +21,6 @@ func (p MongoDB) OffshootSelectors() map[string]string {
 
 func (p MongoDB) OffshootLabels() map[string]string {
 	return filterTags(p.OffshootSelectors(), p.Labels)
-}
-
-func (p MongoDB) StatefulSetAnnotations() map[string]string {
-	annotations := make(map[string]string)
-	for key, val := range p.Annotations {
-		if !strings.HasPrefix(key, GenericKey+"/") && !strings.HasPrefix(key, MongoDBKey+"/") {
-			annotations[key] = val
-		}
-	}
-	return annotations
 }
 
 func (p MongoDB) ResourceShortCode() string {
