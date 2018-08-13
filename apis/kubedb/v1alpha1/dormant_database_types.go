53b3010ccbb27caa9f0191b58eb3ef3aeaab7751
Maruf Tuhin <maruf.2hin@gmail.com>
2018-08-14 05:44:32 +0600

    CRD update to support ObservedGeneration in Status (#273)
    https://github.com/kubedb/project/issues/227

--- apis/kubedb/v1alpha1/dormant_database_types.go
+++ apis/kubedb/v1alpha1/dormant_database_types.go
@@ -81,6 +81,10 @@ type DormantDatabaseStatus struct {
 	WipeOutTime  *metav1.Time         `json:"wipeOutTime,omitempty"`
 	Phase        DormantDatabasePhase `json:"phase,omitempty"`
 	Reason       string               `json:"reason,omitempty"`
+	// observedGeneration is the most recent generation observed for this resource. It corresponds to the
+	// resource's generation, which is updated on mutation by the API Server.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
