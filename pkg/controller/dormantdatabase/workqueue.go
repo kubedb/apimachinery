2e3c8635f733ea8b613276ab8f97bdf7e9b4872e
the-redback <maruf.2hin@gmail.com>
2018-08-14 14:59:35 +0600

    check glevel before calculating diff

--- pkg/controller/dormantdatabase/workqueue.go
+++ pkg/controller/dormantdatabase/workqueue.go
@@ -4,6 +4,7 @@ import (
 	"github.com/appscode/go/log"
 	meta_util "github.com/appscode/kutil/meta"
 	"github.com/appscode/kutil/tools/queue"
+	"github.com/golang/glog"
 	api "github.com/kubedb/apimachinery/apis/kubedb/v1alpha1"
 	"k8s.io/apimachinery/pkg/labels"
 )
@@ -23,15 +24,21 @@ func (c *Controller) addEventHandler(selector labels.Selector) {
 
 func dormantDatabaseEqual(old, new *api.DormantDatabase) bool {
 	if api.EnableStatusSubresource {
-		if new.Status.ObservedGeneration < new.Generation {
-			return false
+		if new.Status.ObservedGeneration >= new.Generation {
+			return true
 		}
-		return true
+		if glog.V(log.LevelDebug) {
+			diff := meta_util.Diff(old, new)
+			glog.InfoDepth(1, "meta.Generation [%d] is higher than status.observedGeneration [%d] in DormantDatabase %s/%s with Diff: %s\n",
+				new.Generation, new.Status.ObservedGeneration, new.Namespace, new.Name, diff)
+		}
+		return false
 	}
-
 	if !meta_util.Equal(old.Spec, new.Spec) {
-		diff := meta_util.Diff(old.Spec, new.Spec)
-		log.Debugf("DormantDatabase %s/%s has changed. Diff: %s\n", new.Namespace, new.Name, diff)
+		if glog.V(log.LevelDebug) {
+			diff := meta_util.Diff(old, new)
+			glog.InfoDepth(1, "DormantDatabase %s/%s has changed. Diff: %s\n", new.Namespace, new.Name, diff)
+		}
 		return false
 	}
 	return true
