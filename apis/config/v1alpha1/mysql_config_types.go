be8660397380cc9418ef7176f4ad28ba8b848c53
nightfury1204 <nahid@appscode.com>
2018-11-09 16:21:26 +0600

    Add plugin name and default method for database configuration

--- apis/config/v1alpha1/mysql_config_types.go
+++ apis/config/v1alpha1/mysql_config_types.go
@@ -15,6 +15,11 @@ type MySQLConfiguration struct {
 	metav1.TypeMeta   `json:",inline,omitempty"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
+	// Specifies the name of the plugin to use for this connection.
+	// Default plugin:
+	//  - for mysql: mysql-database-plugin
+	PluginName string `json:"pluginName,omitempty"`
+
 	// List of the roles allowed to use this connection.
 	// Defaults to empty (no roles), if contains a "*" any role can use this connection.
 	AllowedRoles string `json:"allowedRoles,omitempty"`
