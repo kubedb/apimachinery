ea8d2c0bc6b834c35caeec339f4cbfc2537310aa
the-redback <maruf.2hin@gmail.com>
2018-11-20 10:20:52 +0600

    Probe defaulting for mongodb

--- apis/kubedb/v1alpha1/mongodb_helpers.go
+++ apis/kubedb/v1alpha1/mongodb_helpers.go
@@ -10,6 +10,7 @@ import (
 	"github.com/kubedb/apimachinery/apis"
 	"github.com/kubedb/apimachinery/apis/kubedb"
 	apps "k8s.io/api/apps/v1"
+	core "k8s.io/api/core/v1"
 	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	appcat "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	mona "kmodules.xyz/monitoring-agent-api/api/v1"
@@ -222,6 +223,41 @@ func (m *MongoDBSpec) SetDefaults() {
 			m.TerminationPolicy = TerminationPolicyDelete
 		} else {
 			m.TerminationPolicy = TerminationPolicyPause
+		}
+	}
+	m.SetDefaultProbes()
+}
+
+func (m *MongoDBSpec) SetDefaultProbes() {
+	cmd := []string{
+		"mongo",
+		"--eval",
+		"db.adminCommand('ping')",
+	}
+	if m.PodTemplate.Spec.LivenessProbe == nil {
+		m.PodTemplate.Spec.LivenessProbe = &core.Probe{
+			Handler: core.Handler{
+				Exec: &core.ExecAction{
+					Command: cmd,
+				},
+			},
+			FailureThreshold: 3,
+			PeriodSeconds:    10,
+			SuccessThreshold: 1,
+			TimeoutSeconds:   5,
+		}
+	}
+	if m.PodTemplate.Spec.ReadinessProbe == nil {
+		m.PodTemplate.Spec.ReadinessProbe = &core.Probe{
+			Handler: core.Handler{
+				Exec: &core.ExecAction{
+					Command: cmd,
+				},
+			},
+			FailureThreshold: 3,
+			PeriodSeconds:    10,
+			SuccessThreshold: 1,
+			TimeoutSeconds:   1,
 		}
 	}
 }
