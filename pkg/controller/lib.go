93ee997d13112ef1e11544b1151aed9af4588fc9
the-redback <maruf.2hin@gmail.com>
2019-01-30 11:38:54 +0600

    Add library function GetVolumeForSnapshot to create pvc for snapshot job

--- pkg/controller/lib.go
+++ pkg/controller/lib.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"path/filepath"
 
+	"github.com/appscode/go/log"
 	core_util "github.com/appscode/kutil/core/v1"
 	"github.com/graymeta/stow"
 	_ "github.com/graymeta/stow/azure"
@@ -139,4 +140,59 @@ func (c *Controller) SetJobOwnerReference(snapshot *api.Snapshot, job *batch.Job
 		}
 	}
 	return nil
+}
+
+// GetVolumeForSnapshot returns pvc or empty directory depending on jobpvcspec and dbPvcSpec.
+// In case of PVC, this function will create a PVC then returns the volume.
+func (c *Controller) GetVolumeForSnapshot(st api.StorageType, pvcSpec, jobPvcSpec *core.PersistentVolumeClaimSpec, jobName, namespace string) (*core.Volume, error) {
+	if jobPvcSpec == nil {
+		jobPvcSpec = pvcSpec
+		if st == api.StorageTypeEphemeral {
+			ed := core.EmptyDirVolumeSource{}
+			if jobPvcSpec != nil {
+				if sz, found := jobPvcSpec.Resources.Requests[core.ResourceStorage]; found {
+					ed.SizeLimit = &sz
+				}
+			}
+			return &core.Volume{
+				Name: "util-volume",
+				VolumeSource: core.VolumeSource{
+					EmptyDir: &ed,
+				},
+			}, nil
+		}
+	}
+
+	volume := &core.Volume{
+		Name: "util-volume",
+	}
+	if len(jobPvcSpec.AccessModes) == 0 {
+		jobPvcSpec.AccessModes = []core.PersistentVolumeAccessMode{
+			core.ReadWriteOnce,
+		}
+		log.Infof(`Using "%v" as AccessModes in "%v"`, core.ReadWriteOnce, *jobPvcSpec)
+	}
+
+	claim := &core.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      jobName,
+			Namespace: namespace,
+		},
+		Spec: *jobPvcSpec,
+	}
+	if jobPvcSpec.StorageClassName != nil {
+		claim.Annotations = map[string]string{
+			"volume.beta.kubernetes.io/storage-class": *jobPvcSpec.StorageClassName,
+		}
+	}
+
+	if _, err := c.Client.CoreV1().PersistentVolumeClaims(claim.Namespace).Create(claim); err != nil {
+		return nil, err
+	}
+
+	volume.PersistentVolumeClaim = &core.PersistentVolumeClaimVolumeSource{
+		ClaimName: claim.Name,
+	}
+
+	return volume, nil
 }
