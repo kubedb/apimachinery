f0612d41258448604aa827d3c31cd65a1efc73a8
the-redback <maruf.2hin@gmail.com>
2019-02-06 10:30:34 +0600

    Removed Snapshot creation at the beginning of ScheduleBackup

--- pkg/controller/snapshot/cron.go
+++ pkg/controller/snapshot/cron.go
@@ -101,17 +101,6 @@ func (c *cronController) ScheduleBackup(
 	// Remove previous cron job if exist
 	if id, exists := c.cronEntryIDs.Pop(cronEntryName); exists {
 		c.cron.Remove(id.(cron.EntryID))
-	} else {
-		if err := invoker.createScheduledSnapshot(); err != nil {
-			invoker.eventRecorder.Eventf(
-				invoker.db,
-				core.EventTypeWarning,
-				eventer.EventReasonFailedToList,
-				err.Error(),
-			)
-			log.Errorf(err.Error())
-			return err
-		}
 	}
 
 	// Set cron job
