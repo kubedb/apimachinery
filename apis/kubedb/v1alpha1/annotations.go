9531a0d9abab81a7252400ce587c60eb0fc9604d
shudipta <shudipta@appscode.com>
2019-04-10 18:02:48 +0600

    Add const for default group size and default baSeserverID

--- apis/kubedb/v1alpha1/annotations.go
+++ apis/kubedb/v1alpha1/annotations.go
@@ -42,6 +42,8 @@ const (
 	MySQLMaxGroupMembers = 9
 	// The recommended MySQL server version for group replication (GR)
 	MySQLGRRecommendedVersion = "5.7.25"
+	MySQLDefaultGroupSize = 3
+	MySQLDefaultBaseServerID = 1
 	// The server id for each group member must be unique and in the range [1, 2^32 - 1]
 	// And the maximum group size is 9. So MySQLMaxBaseServerID is the maximum safe value
 	// for BaseServerID calculated as max MySQL server_id value - max Replication Group size.
