03ec2c51f0b5b9b12be81edbb5ff62f8e63f17c3
shudipta <shudipta@appscode.com>
2019-08-29 14:26:31 +0600

    Add ProxySQLVersion type as known type to scheme

--- apis/catalog/v1alpha1/register.go
+++ apis/catalog/v1alpha1/register.go
@@ -53,6 +53,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&RedisVersionList{},
 		&EtcdVersion{},
 		&EtcdVersionList{},
+		&ProxySQLVersion{},
+		&ProxySQLVersionList{},
 	)
 
 	scheme.AddKnownTypes(SchemeGroupVersion,
