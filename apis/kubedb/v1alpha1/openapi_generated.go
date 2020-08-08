2f509c260018fa2eb17658ecf5c53300d5b5a621
lgtm <1gtm@users.noreply.github.com>
2020-08-07 23:56:14 -0700

    Update Kubernetes v1.18.3 dependencies (#561)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- apis/kubedb/v1alpha1/openapi_generated.go
+++ apis/kubedb/v1alpha1/openapi_generated.go
@@ -15841,7 +15841,6 @@ func schema_kmodulesxyz_client_go_api_v1_TLSConfig(ref common.ReferenceCallback)
 						},
 					},
 				},
-				Required: []string{"issuerRef"},
 			},
 		},
 		Dependencies: []string{
