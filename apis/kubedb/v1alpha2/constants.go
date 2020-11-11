3128627078db9074877c1ccaa9b1ee0cc1b55ee2
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2020-11-11 11:51:24 +0600

    Add `ReplicationModeDetector` Image for MongoDB (#645)
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -28,6 +28,10 @@ const (
 	LabelDatabaseName = kubedb.GroupName + "/name"
 	LabelRole         = kubedb.GroupName + "/role"
 
+	ReplicationModeDetectorContainerName = "replication-mode-detector"
+	DatabasePodPrimary                   = "primary"
+	DatabasePodStandby                   = "standby"
+
 	ComponentDatabase     = "database"
 	RoleStats             = "stats"
 	DefaultStatsPath      = "/metrics"
@@ -125,11 +129,6 @@ const (
 	MySQLRootUserName          = "MYSQL_ROOT_USERNAME"
 	MySQLRootPassword          = "MYSQL_ROOT_PASSWORD"
 	MySQLName                  = "MYSQL_NAME"
-
-	MySQLContainerReplicationModeDetectorName = "replication-mode-detector"
-	MySQLPodPrimary                           = "primary"
-	MySQLPodStandby                           = "standby"
-	MySQLLabelRole                            = kubedb.GroupName + "/role"
 
 	MySQLTLSConfigCustom     = "custom"
 	MySQLTLSConfigSkipVerify = "skip-verify"
