df86f5f98720268b450bca022d8e3fa0d3ba1484
Mohammad Fahim Abrar <fahimabrar@appscode.com>
2020-12-11 00:07:50 +0600

    Add condition types
    Signed-off-by: Mohammad Fahim Abrar <fahimabrar@appscode.com>

--- apis/autoscaling/v1alpha1/type.go
+++ apis/autoscaling/v1alpha1/type.go
@@ -22,95 +22,10 @@ import (
 	core "k8s.io/api/core/v1"
 )
 
-// List of possible condition types for a ops request
+// List of possible condition types for a autoscaler
 const (
-	AccessApproved            = "Approved"
-	AccessDenied              = "Denied"
-	DisableSharding           = "DisableSharding"
-	EnableSharding            = "EnableSharding"
-	Failure                   = "Failure"
-	HorizontalScalingDatabase = "HorizontalScaling"
-	MigratingData             = "MigratingData"
-	NodeCreated               = "NodeCreated"
-	NodeDeleted               = "NodeDeleted"
-	NodeRestarted             = "NodeRestarted"
-	PauseDatabase             = "PauseDatabase"
-	PausingDatabase           = "PausingDatabase"
-	PausedDatabase            = "PausedDatabase"
-	Progressing               = "Progressing"
-	ResumeDatabase            = "ResumeDatabase"
-	ResumingDatabase          = "ResumingDatabase"
-	ResumedDatabase           = "ResumedDatabase"
-	ScalingDatabase           = "Scaling"
-	ScalingDown               = "ScalingDown"
-	ScalingUp                 = "ScalingUp"
-	Successful                = "Successful"
-	Updating                  = "Updating"
-	UpgradedVersion           = "UpgradedVersion"
-	UpgradingVersion          = "UpgradingVersion"
-	VerticalScalingDatabase   = "VerticalScaling"
-	VotingExclusionAdded      = "VotingExclusionAdded"
-	VotingExclusionDeleted    = "VotingExclusionDeleted"
-	UpdateStatefulSets        = "UpdateStatefulSets"
-
-	// MongoDB Constants
-	StartingBalancer            = "StartingBalancer"
-	StoppingBalancer            = "StoppingBalancer"
-	UpdateShardImage            = "UpdateShardImage"
-	UpdateStatefulSetResources  = "UpdateStatefulSetResources"
-	UpdateShardResources        = "UpdateShardResources"
-	ScaleDownShard              = "ScaleDownShard"
-	ScaleUpShard                = "ScaleUpShard"
-	UpdateReplicaSetImage       = "UpdateReplicaSetImage"
-	UpdateConfigServerImage     = "UpdateConfigServerImage"
-	UpdateMongosImage           = "UpdateMongosImage"
-	UpdateReplicaSetResources   = "UpdateReplicaSetResources"
-	UpdateConfigServerResources = "UpdateConfigServerResources"
-	UpdateMongosResources       = "UpdateMongosResources"
-	FlushRouterConfig           = "FlushRouterConfig"
-	ScaleDownReplicaSet         = "ScaleDownReplicaSet"
-	ScaleUpReplicaSet           = "ScaleUpReplicaSet"
-	ScaleUpShardReplicas        = "ScaleUpShardReplicas"
-	ScaleDownShardReplicas      = "ScaleDownShardReplicas"
-	ScaleDownConfigServer       = "ScaleDownConfigServer "
-	ScaleUpConfigServer         = "ScaleUpConfigServer "
-	ScaleMongos                 = "ScaleMongos"
-	VolumeExpansion             = "VolumeExpansion"
-)
-
-type AutoscalerPhase string
-
-const (
-	// used for ops requests that are currently Progressing
-	AutoscalerPhaseProgressing AutoscalerPhase = "Progressing"
-	// used for ops requests that are executed successfully
-	AutoscalerPhaseSuccessful AutoscalerPhase = "Successful"
-	// used for ops requests that are waiting for approval
-	AutoscalerPhaseWaitingForApproval AutoscalerPhase = "WaitingForApproval"
-	// used for ops requests that are failed
-	AutoscalerPhaseFailed AutoscalerPhase = "Failed"
-	// used for ops requests that are approved
-	AutoscalerApproved AutoscalerPhase = "Approved"
-	// used for ops requests that are denied
-	AutoscalerDenied AutoscalerPhase = "Denied"
-)
-
-// +kubebuilder:validation:Enum=Upgrade;HorizontalScaling;VerticalScaling;VolumeExpansion;Restart;RotateCertificates
-type AutoscalerType string
-
-const (
-	// used for Upgrade operation
-	AutoscalerTypeUpgrade AutoscalerType = "Upgrade"
-	// used for HorizontalScaling operation
-	AutoscalerTypeHorizontalScaling AutoscalerType = "HorizontalScaling"
-	// used for VerticalScaling operation
-	AutoscalerTypeVerticalScaling AutoscalerType = "VerticalScaling"
-	// used for VolumeExpansion operation
-	AutoscalerTypeVolumeExpansion AutoscalerType = "VolumeExpansion"
-	// used for Restart operation
-	AutoscalerTypeRestart AutoscalerType = "Restart"
-	// used for RotateCertificates operation
-	AutoscalerTypeRotateCertificates AutoscalerType = "RotateCertificates"
+	Failure          = "Failure"
+	CreateOpsRequest = "CreateOpsRequest"
 )
 
 // ContainerControlledValues controls which resource value should be autoscaled.
