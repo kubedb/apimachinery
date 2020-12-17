2e31fb0300d7096520298f113e945800f1e21468
Alif Biswas <alif@appscode.com>
2020-12-17 18:51:28 +0600

    update MariaDBDefaultClusterSize constant
    Signed-off-by: Alif Biswas <alif@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -164,7 +164,7 @@ const (
 	MariaDBClusterRecommendedVersion    = "10.5"
 	MariaDBMaxClusterNameLength         = 32
 	MariaDBStandaloneReplicas           = 1
-	MariaDBDefaultClusterSize           = 2
+	MariaDBDefaultClusterSize           = 3
 	MariaDBDataMountPath                = "/var/lib/mysql"
 	MariaDBDataLostFoundPath            = MariaDBDataMountPath + "lost+found"
 	MariaDBInitDBMountPath              = "/docker-entrypoint-initdb.d"
