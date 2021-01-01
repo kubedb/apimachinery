31584e580dec0e80731ea50fafb8772d86fb5f1e
Sayf Uddin Al Azad Sagor <33339455+suaas21@users.noreply.github.com>
2021-01-01 14:06:07 +0600

    Add DB constants and tls-reconfigure checker func (#657)
    Signed-off-by: suaas21 <sagor@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -47,6 +47,9 @@ const (
 	DBCustomConfigName             = "custom-config"
 	DefaultVolumeClaimTemplateName = "data"
 
+	DBTLSVolume         = "tls-volume"
+	DBExporterTLSVolume = "exporter-tls-volume"
+
 	// =========================== Database key Constants ============================
 	PostgresKey      = ResourceSingularPostgres + "." + kubedb.GroupName
 	ElasticsearchKey = ResourceSingularElasticsearch + "." + kubedb.GroupName
