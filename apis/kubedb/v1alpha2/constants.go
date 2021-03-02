201456e8ae83b88ffd7c02cce3f263e1876bfa74
Habibur Rahaman Emon <32496488+Emon46@users.noreply.github.com>
2021-03-02 16:57:43 +0600

    Postgres : updated leader elector [ElectionTick,HeartbeatTick] (#700)
    Signed-off-by: Emon46 <emon@appscode.com>
    Co-authored-by: Tamal Saha <tamal@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -200,13 +200,34 @@ const (
 	MariaDBClusterCustomConfigMountPath = "/etc/percona-xtradb-cluster.conf.d/"
 
 	// =========================== PostgreSQL Constants ============================
-	PostgresDatabasePortName       = "db"
-	PostgresPrimaryServicePortName = "primary"
-	PostgresStandbyServicePortName = "standby"
-	PostgresDatabasePort           = 5432
-	PostgresPodPrimary             = "primary"
-	PostgresPodStandby             = "standby"
-	PostgresLabelRole              = kubedb.GroupName + "/role"
+	PostgresDatabasePortName             = "db"
+	PostgresPrimaryServicePortName       = "primary"
+	PostgresStandbyServicePortName       = "standby"
+	PostgresDatabasePort                 = 5432
+	PostgresPodPrimary                   = "primary"
+	PostgresPodStandby                   = "standby"
+	PostgresLabelRole                    = kubedb.GroupName + "/role"
+	PostgresLeaderElectionContainerName  = "leader-elector"
+	PostgresLeaderElectionPort           = 12345
+	PostgresLeaderElectionPortName       = "leaderelection"
+	PostgresLeaderElectionClientPort     = 12380
+	PostgresLeaderElectionClientPortName = "leaderapiclient"
+	PostgresRunScriptMountPath           = "/run_scripts"
+	PostgresRunScriptVolumeName          = "scripts"
+
+	PostgresCurrentXlogLocation     = "pg_current_xlog_location"
+	PostgresLastXlogReceiveLocation = "pg_last_xlog_receive_location"
+	PostgresLastXlogReplayLocation  = "pg_last_xlog_replay_location"
+	PostgresXlogLocationDiff        = "pg_xlog_location_diff"
+
+	PostgresCurrentWalLSN         = "pg_current_wal_lsn"
+	PostgresLastWalReceivePostion = "pg_last_wal_receive_lsn"
+	PostgresLastWalReplayLSN      = "pg_last_wal_replay_lsn"
+	PostgresWalLSNDiff            = "pg_wal_lsn_diff"
+
+	PostgresKeyFileSecretSuffix = "key"
+	PostgresPEMSecretSuffix     = "pem"
+	PostgresRootUsername        = "root"
 
 	// =========================== ProxySQL Constants ============================
 	LabelProxySQLName        = ProxySQLKey + "/name"
