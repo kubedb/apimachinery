fd9f49513f0b9d78db0d4e35384ee0fbca356caa
Md. Alif Biswas <alif@appscode.com>
2021-03-25 13:08:35 +0600

    Added MariaDBTLSConfigCustom constant
    Signed-off-by: Md. Alif Biswas <alif@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -201,6 +201,7 @@ const (
 	MariaDBInitDBMountPath              = "/docker-entrypoint-initdb.d"
 	MariaDBCustomConfigMountPath        = "/etc/percona-server.conf.d/"
 	MariaDBClusterCustomConfigMountPath = "/etc/percona-xtradb-cluster.conf.d/"
+	MariaDBTLSConfigCustom              = "custom"
 
 	// =========================== PostgreSQL Constants ============================
 	PostgresDatabasePortName       = "db"
