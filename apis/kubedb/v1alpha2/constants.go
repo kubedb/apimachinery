24d44217f02f5af5f741d931788d721aee9b40f8
Habibur Rahaman Emon <emon@appscode.com>
2021-11-02 22:45:35 +0600

    Update default resource for pg-coordinator (#813)
    Signed-off-by: Emon46 <emon@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -360,12 +360,11 @@ var (
 	}
 	// CoordinatorDefaultResources must be used for raft backed coordinators to avoid unintended leader switches
 	CoordinatorDefaultResources = core.ResourceRequirements{
-		Limits: core.ResourceList{
-			core.ResourceCPU:    resource.MustParse(".500"),
+		Requests: core.ResourceList{
+			core.ResourceCPU:    resource.MustParse(".200"),
 			core.ResourceMemory: resource.MustParse("256Mi"),
 		},
-		Requests: core.ResourceList{
-			core.ResourceCPU:    resource.MustParse(".500"),
+		Limits: core.ResourceList{
 			core.ResourceMemory: resource.MustParse("256Mi"),
 		},
 	}
