105e40b72a435e7209adab9a477dd6436a8bcb09
Md. Alif Biswas <alif@appscode.com>
2022-01-20 19:40:38 +0600

    Add apply config on MariaDB Reconfigure Ops Request
    Signed-off-by: Md. Alif Biswas <alif@appscode.com>

--- apis/ops/v1alpha1/zz_generated.deepcopy.go
+++ apis/ops/v1alpha1/zz_generated.deepcopy.go
@@ -869,6 +869,13 @@ func (in *MariaDBCustomConfigurationSpec) DeepCopyInto(out *MariaDBCustomConfigu
 		*out = new(v1.LocalObjectReference)
 		**out = **in
 	}
+	if in.ApplyConfig != nil {
+		in, out := &in.ApplyConfig, &out.ApplyConfig
+		*out = make(map[string]string, len(*in))
+		for key, val := range *in {
+			(*out)[key] = val
+		}
+	}
 	return
 }
 
