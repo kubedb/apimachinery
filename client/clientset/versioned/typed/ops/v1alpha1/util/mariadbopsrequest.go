0e0c25bf86cf67cc922f0eaa84fba804a5149401
Arnob kumar saha <arnob@appscode.com>
2022-05-19 19:12:10 +0600

    Add utils in clientset
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

---
+++ client/clientset/versioned/typed/ops/v1alpha1/util/mariadbopsrequest.go
@@ -0,0 +1,135 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package util
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	api "kubedb.dev/apimachinery/apis/ops/v1alpha1"
+	cs "kubedb.dev/apimachinery/client/clientset/versioned/typed/ops/v1alpha1"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/klog/v2"
+	kutil "kmodules.xyz/client-go"
+)
+
+func CreateOrPatchMariaDBOpsRequest(ctx context.Context, c cs.OpsV1alpha1Interface, meta metav1.ObjectMeta, transform func(*api.MariaDBOpsRequest) *api.MariaDBOpsRequest, opts metav1.PatchOptions) (*api.MariaDBOpsRequest, kutil.VerbType, error) {
+	cur, err := c.MariaDBOpsRequests(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+	if kerr.IsNotFound(err) {
+		klog.V(3).Infof("Creating MariaDBOpsRequest %s/%s.", meta.Namespace, meta.Name)
+		out, err := c.MariaDBOpsRequests(meta.Namespace).Create(ctx, transform(&api.MariaDBOpsRequest{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "MariaDBOpsRequest",
+				APIVersion: api.SchemeGroupVersion.String(),
+			},
+			ObjectMeta: meta,
+		}), metav1.CreateOptions{
+			DryRun:       opts.DryRun,
+			FieldManager: opts.FieldManager,
+		})
+		return out, kutil.VerbCreated, err
+	} else if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	return PatchMariaDBOpsRequest(ctx, c, cur, transform, opts)
+}
+
+func PatchMariaDBOpsRequest(ctx context.Context, c cs.OpsV1alpha1Interface, cur *api.MariaDBOpsRequest, transform func(*api.MariaDBOpsRequest) *api.MariaDBOpsRequest, opts metav1.PatchOptions) (*api.MariaDBOpsRequest, kutil.VerbType, error) {
+	return PatchMariaDBOpsRequestObject(ctx, c, cur, transform(cur.DeepCopy()), opts)
+}
+
+func PatchMariaDBOpsRequestObject(ctx context.Context, c cs.OpsV1alpha1Interface, cur, mod *api.MariaDBOpsRequest, opts metav1.PatchOptions) (*api.MariaDBOpsRequest, kutil.VerbType, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	modJson, err := json.Marshal(mod)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+
+	patch, err := jsonpatch.CreateMergePatch(curJson, modJson)
+	if err != nil {
+		return nil, kutil.VerbUnchanged, err
+	}
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, kutil.VerbUnchanged, nil
+	}
+	klog.V(3).Infof("Patching MariaDBOpsRequest %s/%s with %s.", cur.Namespace, cur.Name, string(patch))
+	out, err := c.MariaDBOpsRequests(cur.Namespace).Patch(ctx, cur.Name, types.MergePatchType, patch, opts)
+	return out, kutil.VerbPatched, err
+}
+
+func UpdateMariaDBOpsRequestStatus(
+	ctx context.Context,
+	c cs.OpsV1alpha1Interface,
+	meta metav1.ObjectMeta,
+	transform func(*api.MariaDBOpsRequestStatus) (types.UID, *api.MariaDBOpsRequestStatus),
+	opts metav1.UpdateOptions,
+) (result *api.MariaDBOpsRequest, err error) {
+	apply := func(x *api.MariaDBOpsRequest) *api.MariaDBOpsRequest {
+		uid, updatedStatus := transform(x.Status.DeepCopy())
+		// Ignore status update when uid does not match
+		if uid != "" && uid != x.UID {
+			return x
+		}
+		return &api.MariaDBOpsRequest{
+			TypeMeta:   x.TypeMeta,
+			ObjectMeta: x.ObjectMeta,
+			Spec:       x.Spec,
+			Status:     *updatedStatus,
+		}
+	}
+
+	attempt := 0
+	cur, err := c.MariaDBOpsRequests(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	err = wait.PollImmediate(kutil.RetryInterval, kutil.RetryTimeout, func() (bool, error) {
+		attempt++
+		var e2 error
+		result, e2 = c.MariaDBOpsRequests(meta.Namespace).UpdateStatus(ctx, apply(cur), opts)
+		if kerr.IsConflict(e2) {
+			latest, e3 := c.MariaDBOpsRequests(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+			switch {
+			case e3 == nil:
+				cur = latest
+				return false, nil
+			case kutil.IsRequestRetryable(e3):
+				return false, nil
+			default:
+				return false, e3
+			}
+		} else if err != nil && !kutil.IsRequestRetryable(e2) {
+			return false, e2
+		}
+		return e2 == nil, nil
+	})
+
+	if err != nil {
+		err = fmt.Errorf("failed to update status of MariaDBOpsRequest %s/%s after %d attempts due to %v", meta.Namespace, meta.Name, attempt, err)
+	}
+	return
+}
