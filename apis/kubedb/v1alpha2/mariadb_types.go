3b5266c0ea862d1102750079ce9480b6735287ee
Md. Alif Biswas <alif@appscode.com>
2022-12-12 17:29:35 +0600

    Add MariaDB Archiver API
    Signed-off-by: Md. Alif Biswas <alif@appscode.com>

--- apis/kubedb/v1alpha2/mariadb_types.go
+++ apis/kubedb/v1alpha2/mariadb_types.go
@@ -129,6 +129,10 @@ type MariaDBSpec struct {
 	// +optional
 	// +kubebuilder:default={periodSeconds: 10, timeoutSeconds: 10, failureThreshold: 1}
 	HealthChecker kmapi.HealthCheckSpec `json:"healthChecker"`
+
+	// Archiver controls database backup using Archiver CR
+	// +optional
+	Archiver *Archiver `json:"archiver,omitempty"`
 }
 
 // +kubebuilder:validation:Enum=server;archiver;metrics-exporter
