7f6ffca9e87df5299c914222b7fd99efe9a53aea
Tamal Saha <tamal@appscode.com>
2022-12-23 11:45:35 +0600

    Revise PgBouncer api (#1002)
    Co-authored-by: Rakibul-Hossain <rakibul.hossain@appscode.com>
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/kubedb/v1alpha2/constants.go
+++ apis/kubedb/v1alpha2/constants.go
@@ -439,19 +439,21 @@ const (
 	EnvRedisPassword         = "REDISCLI_AUTH"
 
 	// =========================== PgBouncer Constants ============================
-	PgBouncerUpstreamServerCA         = "upstream-server-ca.crt"
-	PgBouncerUpstreamServerClientCert = "upstream-server-client.crt"
-	PgBouncerUpstreamServerClientKey  = "upstream-server-client.key"
-	PgBouncerClientCrt                = "client.crt"
-	PgBouncerClientKey                = "client.key"
-	PgBouncerCACrt                    = "ca.crt"
-	PgBouncerTLSCrt                   = "tls.crt"
-	PgBouncerTLSKey                   = "tls.key"
-	PgBouncerDatabasePortName         = "db"
-	PgBouncerPrimaryServicePortName   = "primary"
-	PgBouncerDatabasePort             = 5432
-	PgBouncerConfigFile               = "pgbouncer.ini"
-	PgBouncerAdminUsername            = "kubedb"
+	PgBouncerUpstreamServerCA               = "upstream-server-ca.crt"
+	PgBouncerUpstreamServerClientCert       = "upstream-server-client.crt"
+	PgBouncerUpstreamServerClientKey        = "upstream-server-client.key"
+	PgBouncerClientCrt                      = "client.crt"
+	PgBouncerClientKey                      = "client.key"
+	PgBouncerCACrt                          = "ca.crt"
+	PgBouncerTLSCrt                         = "tls.crt"
+	PgBouncerTLSKey                         = "tls.key"
+	PgBouncerDatabasePortName               = "db"
+	PgBouncerPrimaryServicePortName         = "primary"
+	PgBouncerDatabasePort                   = 5432
+	PgBouncerConfigFile                     = "pgbouncer.ini"
+	PgBouncerAdminUsername                  = "pgbouncer"
+	PgBouncerDefaultPoolMode                = "session"
+	PgBouncerDefaultIgnoreStartupParameters = "empty"
 )
 
 // List of possible condition types for a KubeDB object
