60f139d87fb5a24c655cf513f6e8a931ac3c974d
hmsayem <hmsayem@appscode.com>
2024-01-17 17:59:33 +0600

    Run make gen
    Signed-off-by: hmsayem <hmsayem@appscode.com>

--- client/clientset/versioned/typed/archiver/v1alpha1/fake/fake_mariadbarchiver.go
+++ client/clientset/versioned/typed/archiver/v1alpha1/fake/fake_mariadbarchiver.go
@@ -25,7 +25,6 @@ import (
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
 	types "k8s.io/apimachinery/pkg/types"
 	watch "k8s.io/apimachinery/pkg/watch"
 	testing "k8s.io/client-go/testing"
@@ -37,9 +36,9 @@ type FakeMariaDBArchivers struct {
 	ns   string
 }
 
-var mariadbarchiversResource = schema.GroupVersionResource{Group: "archiver.kubedb.com", Version: "v1alpha1", Resource: "mariadbarchivers"}
+var mariadbarchiversResource = v1alpha1.SchemeGroupVersion.WithResource("mariadbarchivers")
 
-var mariadbarchiversKind = schema.GroupVersionKind{Group: "archiver.kubedb.com", Version: "v1alpha1", Kind: "MariaDBArchiver"}
+var mariadbarchiversKind = v1alpha1.SchemeGroupVersion.WithKind("MariaDBArchiver")
 
 // Get takes name of the mariaDBArchiver, and returns the corresponding mariaDBArchiver object, and an error if there is any.
 func (c *FakeMariaDBArchivers) Get(ctx context.Context, name string, options v1.GetOptions) (result *v1alpha1.MariaDBArchiver, err error) {
