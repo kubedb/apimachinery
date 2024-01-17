8ccd211525de75f289b243fb06dd536145ba3c3a
pritamdas99 <pritam@appscode.com>
2024-01-17 19:59:03 +0600

    Apply make fmt.
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- client/clientset/versioned/typed/kubedb/v1alpha1/kubedb_client.go
+++ client/clientset/versioned/typed/kubedb/v1alpha1/kubedb_client.go
@@ -21,10 +21,9 @@ package v1alpha1
 import (
 	"net/http"
 
+	rest "k8s.io/client-go/rest"
 	v1alpha1 "kubedb.dev/apimachinery/apis/kubedb/v1alpha1"
 	"kubedb.dev/apimachinery/client/clientset/versioned/scheme"
-
-	rest "k8s.io/client-go/rest"
 )
 
 type KubedbV1alpha1Interface interface {
