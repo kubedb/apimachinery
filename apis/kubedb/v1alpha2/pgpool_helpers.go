81bc536a0582df93a477210e9b17f43a2ecc0d3c
MobarakHsn <mobarak@appscode.com>
2024-02-22 14:25:59 +0600

    Pgpool security context and default resource
    Signed-off-by: MobarakHsn <mobarak@appscode.com>

--- apis/kubedb/v1alpha2/pgpool_helpers.go
+++ apis/kubedb/v1alpha2/pgpool_helpers.go
@@ -19,13 +19,13 @@ package v1alpha2
 import (
 	"context"
 	"fmt"
-	promapi "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
-	mona "kmodules.xyz/monitoring-agent-api/api/v1"
 
+	"kubedb.dev/apimachinery/apis"
 	catalog "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
 	"kubedb.dev/apimachinery/apis/kubedb"
 	"kubedb.dev/apimachinery/crds"
 
+	promapi "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"gomodules.xyz/pointer"
 	core "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -34,7 +34,10 @@ import (
 	appslister "k8s.io/client-go/listers/apps/v1"
 	"k8s.io/klog/v2"
 	"kmodules.xyz/client-go/apiextensions"
+	core_util "kmodules.xyz/client-go/core/v1"
 	meta_util "kmodules.xyz/client-go/meta"
+	"kmodules.xyz/client-go/policy/secomp"
+	mona "kmodules.xyz/monitoring-agent-api/api/v1"
 	ofst "kmodules.xyz/offshoot-api/api/v2"
 )
 
@@ -200,26 +203,77 @@ func (p *Pgpool) ServiceLabels(alias ServiceAlias, extraLabels ...map[string]str
 	return p.offshootLabels(meta_util.OverwriteKeys(p.OffshootSelectors(), extraLabels...), svcTemplate.Labels)
 }
 
-func (p *Pgpool) SetSecurityContext(ppVersion *catalog.PgpoolVersion) {
-	if p.Spec.PodTemplate.Spec.SecurityContext == nil {
-		p.Spec.PodTemplate.Spec.SecurityContext = &core.PodSecurityContext{
-			RunAsUser:    ppVersion.Spec.SecurityContext.RunAsUser,
-			RunAsGroup:   ppVersion.Spec.SecurityContext.RunAsUser,
-			RunAsNonRoot: pointer.BoolP(true),
-		}
-	} else {
-		if p.Spec.PodTemplate.Spec.SecurityContext.RunAsUser == nil {
-			p.Spec.PodTemplate.Spec.SecurityContext.RunAsUser = ppVersion.Spec.SecurityContext.RunAsUser
-		}
-		if p.Spec.PodTemplate.Spec.SecurityContext.RunAsGroup == nil {
-			p.Spec.PodTemplate.Spec.SecurityContext.RunAsGroup = p.Spec.PodTemplate.Spec.SecurityContext.RunAsUser
-		}
+func (p *Pgpool) SetSecurityContext(ppVersion *catalog.PgpoolVersion, podTemplate *ofst.PodTemplateSpec) {
+	if podTemplate == nil {
+		return
+	}
+	if podTemplate.Spec.SecurityContext == nil {
+		podTemplate.Spec.SecurityContext = &core.PodSecurityContext{}
+	}
+	if podTemplate.Spec.SecurityContext.FSGroup == nil {
+		podTemplate.Spec.SecurityContext.FSGroup = ppVersion.Spec.SecurityContext.RunAsUser
 	}
 
-	// Need to set FSGroup equal to  p.Spec.PodTemplate.Spec.SecurityContext.RunAsGroup.
-	// So that /var/pv directory have the group permission for the RunAsGroup user GID.
-	// Otherwise, We will get write permission denied.
-	p.Spec.PodTemplate.Spec.SecurityContext.FSGroup = p.Spec.PodTemplate.Spec.SecurityContext.RunAsGroup
+	container := core_util.GetContainerByName(podTemplate.Spec.Containers, PgpoolContainerName)
+	if container == nil {
+		container = &core.Container{
+			Name: PgpoolContainerName,
+		}
+	}
+	if container.SecurityContext == nil {
+		container.SecurityContext = &core.SecurityContext{}
+	}
+	p.assignContainerSecurityContext(ppVersion, container.SecurityContext)
+	podTemplate.Spec.Containers = core_util.UpsertContainer(podTemplate.Spec.Containers, *container)
+
+	//if p.Spec.Monitor != nil {
+	//	container = core_util.GetContainerByName(podTemplate.Spec.Containers, mona.PrometheusExporterPortName)
+	//	if container == nil {
+	//		container = &core.Container{
+	//			Name: mona.PrometheusExporterPortName,
+	//		}
+	//	}
+	//	if container.SecurityContext == nil {
+	//		container.SecurityContext = &core.SecurityContext{}
+	//	}
+	//	p.assignContainerSecurityContext(ppVersion, container.SecurityContext)
+	//	podTemplate.Spec.Containers = core_util.UpsertContainer(podTemplate.Spec.Containers, *container)
+	//}
+}
+
+func (p *Pgpool) assignContainerSecurityContext(ppVersion *catalog.PgpoolVersion, sc *core.SecurityContext) {
+	if sc.AllowPrivilegeEscalation == nil {
+		sc.AllowPrivilegeEscalation = pointer.BoolP(false)
+	}
+	if sc.Capabilities == nil {
+		sc.Capabilities = &core.Capabilities{
+			Drop: []core.Capability{"ALL"},
+		}
+	}
+	if sc.RunAsNonRoot == nil {
+		sc.RunAsNonRoot = pointer.BoolP(true)
+	}
+	if sc.RunAsUser == nil {
+		sc.RunAsUser = ppVersion.Spec.SecurityContext.RunAsUser
+	}
+	if sc.RunAsGroup == nil {
+		sc.RunAsGroup = ppVersion.Spec.SecurityContext.RunAsUser
+	}
+	if sc.SeccompProfile == nil {
+		sc.SeccompProfile = secomp.DefaultSeccompProfile()
+	}
+}
+
+func (p *Pgpool) setContainerResourceLimits(podTemplate *ofst.PodTemplateSpec) {
+	ppContainer := core_util.GetContainerByName(podTemplate.Spec.Containers, PgpoolContainerName)
+	if ppContainer != nil && (ppContainer.Resources.Requests == nil && ppContainer.Resources.Limits == nil) {
+		apis.SetDefaultResourceLimits(&ppContainer.Resources, DefaultResources)
+	}
+
+	//exporterContainer := core_util.GetContainerByName(podTemplate.Spec.Containers, mona.PrometheusExporterPortName)
+	//if exporterContainer != nil && (exporterContainer.Resources.Requests == nil && exporterContainer.Resources.Limits == nil) {
+	//	apis.SetDefaultResourceLimits(&exporterContainer.Resources, DefaultResources)
+	//}
 }
 
 func (p *Pgpool) SetDefaults() {
@@ -245,12 +299,10 @@ func (p *Pgpool) SetDefaults() {
 		klog.Errorf("can't get the pgpool version object %s for %s \n", err.Error(), p.Spec.Version)
 		return
 	}
-	if p.Spec.PodTemplate != nil {
-		p.SetSecurityContext(&ppVersion)
-	}
 
-	p.SetHealthCheckerDefaults()
-	p.Spec.Monitor.SetDefaults()
+	if p.Spec.Monitor != nil {
+		p.Spec.Monitor.SetDefaults()
+	}
 	if p.Spec.Monitor != nil && p.Spec.Monitor.Prometheus != nil {
 		if p.Spec.Monitor.Prometheus.Exporter.SecurityContext.RunAsUser == nil {
 			p.Spec.Monitor.Prometheus.Exporter.SecurityContext.RunAsUser = ppVersion.Spec.SecurityContext.RunAsUser
@@ -259,6 +311,10 @@ func (p *Pgpool) SetDefaults() {
 			p.Spec.Monitor.Prometheus.Exporter.SecurityContext.RunAsGroup = p.Spec.Monitor.Prometheus.Exporter.SecurityContext.RunAsUser
 		}
 	}
+
+	p.SetHealthCheckerDefaults()
+	p.SetSecurityContext(&ppVersion, p.Spec.PodTemplate)
+	p.setContainerResourceLimits(p.Spec.PodTemplate)
 }
 
 func (p *Pgpool) GetPersistentSecrets() []string {
