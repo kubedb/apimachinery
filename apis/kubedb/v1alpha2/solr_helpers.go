9729f186f604a7d4eb5973526472019456a07817
pritamdas99 <pritam@appscode.com>
2024-02-23 18:31:44 +0600

    Set memory intensive default resource for main container.
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- apis/kubedb/v1alpha2/solr_helpers.go
+++ apis/kubedb/v1alpha2/solr_helpers.go
@@ -318,7 +318,7 @@ func (s *Solr) assignDefaultContainerSecurityContext(slVersion *catalog.SolrVers
 func (s *Solr) setDefaultContainerResourceLimits(podTemplate *ofst.PodTemplateSpec) {
 	dbContainer := coreutil.GetContainerByName(podTemplate.Spec.Containers, SolrContainerName)
 	if dbContainer != nil && (dbContainer.Resources.Requests == nil && dbContainer.Resources.Limits == nil) {
-		apis.SetDefaultResourceLimits(&dbContainer.Resources, DefaultResources)
+		apis.SetDefaultResourceLimits(&dbContainer.Resources, DefaultResourcesMemoryIntensive)
 	}
 
 	initContainer := coreutil.GetContainerByName(podTemplate.Spec.InitContainers, SolrInitContainerName)
