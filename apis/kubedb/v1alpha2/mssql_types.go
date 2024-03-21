5cdd5f7915516f3cf0bd5760d1924b4246ea3082
souravbiswassanto <saurov@appscode.com>
2024-03-21 17:38:57 +0600

    Add internal authentication mode
    Signed-off-by: souravbiswassanto <saurov@appscode.com>

--- apis/kubedb/v1alpha2/mssql_types.go
+++ apis/kubedb/v1alpha2/mssql_types.go
@@ -83,6 +83,10 @@ type MsSQLSpec struct {
 	// +optional
 	AuthSecret *SecretReference `json:"authSecret,omitempty"`
 
+	// InternalAuth is used to authenticate endpoint
+	// +optional
+	InternalAuth *InternalAuthentication `json:"internalAuth"`
+
 	// Init is used to initialize database
 	// +optional
 	Init *InitSpec `json:"init,omitempty"`
@@ -111,6 +115,12 @@ type MsSQLSpec struct {
 	// +optional
 	// +kubebuilder:default={periodSeconds: 10, timeoutSeconds: 10, failureThreshold: 1}
 	HealthChecker kmapi.HealthCheckSpec `json:"healthChecker"`
+}
+
+// InternalAuthentication provides different way of endpoint authentication
+type InternalAuthentication struct {
+	// EndpointCert is used for endpoint authentication of MSSql Server
+	EndpointCert *kmapi.TLSConfig `json:"endpointCert"`
 }
 
 type MsSQLTopology struct {
