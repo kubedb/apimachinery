3a514a1b8f4b53d173bca42ecd9b120821feb372
Neaj Morshad <neaj@appscode.com>
2024-06-03 17:33:45 +0600

    Update deps
    Signed-off-by: Neaj Morshad <neaj@appscode.com>

--- apis/kubedb/v1alpha2/zz_generated.deepcopy.go
+++ apis/kubedb/v1alpha2/zz_generated.deepcopy.go
@@ -2182,6 +2182,16 @@ func (in *MSSQLServerSpec) DeepCopyInto(out *MSSQLServerSpec) {
 		(*in).DeepCopyInto(*out)
 	}
 	in.HealthChecker.DeepCopyInto(&out.HealthChecker)
+	if in.Monitor != nil {
+		in, out := &in.Monitor, &out.Monitor
+		*out = new(monitoringagentapiapiv1.AgentSpec)
+		(*in).DeepCopyInto(*out)
+	}
+	if in.PodPlacementPolicy != nil {
+		in, out := &in.PodPlacementPolicy, &out.PodPlacementPolicy
+		*out = new(corev1.LocalObjectReference)
+		**out = **in
+	}
 	return
 }
 
