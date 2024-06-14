18a87194390713ee79032fbb9da5bb5cd32f1b3a
Anisur Rahman <anisur@appscode.com>
2024-06-14 18:57:19 +0600

    Init api types of mssqlserver archiver

---
+++ apis/archiver/v1alpha1/mssqlserverarchiver_types.go
@@ -0,0 +1,79 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kmapi "kmodules.xyz/client-go/api/v1"
+	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
+)
+
+const (
+	ResourceKindMSSQLServerArchiver     = "MSSQLServerArchiver"
+	ResourceSingularMSSQLServerArchiver = "mssqlserverarchiver"
+	ResourcePluralMSSQLServerArchiver   = "mssqlserverarchivers"
+)
+
+// +genclient
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:path=mssqlserverarchivers,singular=mssqlserverarchiver,shortName=msarchiver,categories={archiver,kubedb,appscode}
+// +kubebuilder:subresource:status
+type MSSQLServerArchiver struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   MSSQLServerArchiverSpec   `json:"spec,omitempty"`
+	Status MSSQLServerArchiverStatus `json:"status,omitempty"`
+}
+
+// MSSQLServerArchiverSpec defines the desired state of MSSQLServerArchiver
+type MSSQLServerArchiverSpec struct {
+	// Databases define which MSSQLServer databases are allowed to consume this archiver
+	Databases *dbapi.AllowedConsumers `json:"databases"`
+	// Pause defines if the backup process should be paused or not
+	// +optional
+	Pause bool `json:"pause,omitempty"`
+	// RetentionPolicy field is the RetentionPolicy of the backupConfiguration's backend
+	// +optional
+	RetentionPolicy *kmapi.ObjectReference `json:"retentionPolicy"`
+	// FullBackup defines the sessionConfig of the fullBackup
+	// This options will eventually go to the full-backup job's yaml
+	// +optional
+	FullBackup *FullBackupOptions `json:"fullBackup"`
+	// WalBackup defines the sessionConfig of the walBackup
+	// This options will eventually go to the sidekick specification
+	// +optional
+	WalBackup *WalBackupOptions `json:"walBackup"`
+	// ManifestBackup defines the sessionConfig of the manifestBackup
+	// This options will eventually go to the manifest-backup job's yaml
+	// +optional
+	ManifestBackup *ManifestBackupOptions `json:"manifestBackup"`
+	// +optional
+	EncryptionSecret *kmapi.ObjectReference `json:"encryptionSecret"`
+	// BackupStorage is the backend storageRef of the BackupConfiguration
+	// +optional
+	BackupStorage *BackupStorage `json:"backupStorage"`
+	// DeletionPolicy defines the created repository's deletionPolicy
+	// +optional
+	DeletionPolicy *DeletionPolicy `json:"deletionPolicy"`
+}
+
+// MSSQLServerArchiverStatus defines the observed state of MSSQLServerArchiver
+type MSSQLServerArchiverStatus struct {
+	// Specifies the information of all the databases managed by this archiver
+	// +optional
+	DatabaseRefs []ArchiverDatabaseRef `json:"databaseRefs,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type MSSQLServerArchiverList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []MSSQLServerArchiver `json:"items"`
+}
+
+//func init() {
+//	SchemeBuilder.Register(&MSSQLServerArchiver{}, &MSSQLServerArchiverList{})
+//}
