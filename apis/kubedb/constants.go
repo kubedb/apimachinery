27f963746166e2edbea8de562e3e9e283c397d45
sayedppqq <sayedmmnn0@gmail.com>
2024-10-21 16:01:15 +0600

    update default resources for mongodb version 8
    Signed-off-by: sayedppqq <sayed@appscode.com>

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -1607,21 +1607,31 @@ var (
 			core.ResourceStorage: resource.MustParse("2Gi"),
 			// these are the default cpu & memory for a coordinator container
 			core.ResourceCPU:    resource.MustParse(".200"),
-			core.ResourceMemory: resource.MustParse("256Mi"),
+			core.ResourceMemory: resource.MustParse("500Mi"),
 		},
 		Limits: core.ResourceList{
-			core.ResourceMemory: resource.MustParse("256Mi"),
+			core.ResourceMemory: resource.MustParse("500Mi"),
 		},
 	}
 
-	// DefaultResourcesCPUIntensive is for MongoDB versions >= 6
-	DefaultResourcesCPUIntensive = core.ResourceRequirements{
+	// DefaultResourcesCPUIntensiveVersion6 is for MongoDB versions >= 6
+	DefaultResourcesCPUIntensiveVersion6 = core.ResourceRequirements{
 		Requests: core.ResourceList{
 			core.ResourceCPU:    resource.MustParse(".800"),
 			core.ResourceMemory: resource.MustParse("1024Mi"),
 		},
 		Limits: core.ResourceList{
 			core.ResourceMemory: resource.MustParse("1024Mi"),
+		},
+	}
+	// DefaultResourcesCPUIntensiveVersion8 is for MongoDB versions >= 8
+	DefaultResourcesCPUIntensiveVersion8 = core.ResourceRequirements{
+		Requests: core.ResourceList{
+			core.ResourceCPU:    resource.MustParse(".800"),
+			core.ResourceMemory: resource.MustParse("1.5Gi"),
+		},
+		Limits: core.ResourceList{
+			core.ResourceMemory: resource.MustParse("1.5Gi"),
 		},
 	}
 
