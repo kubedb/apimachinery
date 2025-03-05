1b40b4dadf653a353ab8ac7e158421e528ada3f8
pritamdas99 <pritam@appscode.com>
2025-03-05 14:16:54 +0600

    Add hazelcast api
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- client/clientset/versioned/typed/catalog/v1alpha1/generated_expansion.go
+++ client/clientset/versioned/typed/catalog/v1alpha1/generated_expansion.go
@@ -30,6 +30,8 @@ type EtcdVersionExpansion interface{}
 
 type FerretDBVersionExpansion interface{}
 
+type HazelcastVersionExpansion interface{}
+
 type KafkaConnectorVersionExpansion interface{}
 
 type KafkaVersionExpansion interface{}
--- client/informers/externalversions/generic.go
+++ client/informers/externalversions/generic.go
@@ -136,6 +136,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().EtcdVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("ferretdbversions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().FerretDBVersions().Informer()}, nil
+	case catalogv1alpha1.SchemeGroupVersion.WithResource("hazelcastversions"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().HazelcastVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("kafkaconnectorversions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().KafkaConnectorVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("kafkaversions"):
@@ -268,6 +270,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Etcds().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("ferretdbs"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().FerretDBs().Informer()}, nil
+	case v1alpha2.SchemeGroupVersion.WithResource("hazelcasts"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Hazelcasts().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("kafkas"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Kafkas().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("mssqlservers"):
