3353f7af76ff94a7adcdc8e758ca6dff5ad09cc1
SK Ali Arman <arman@appscode.com>
2025-03-11 12:13:49 +0600

    update clickhouse webhook
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- webhooks/kubedb/v1alpha2/clickhouse.go
+++ webhooks/kubedb/v1alpha2/clickhouse.go
@@ -20,134 +20,163 @@ import (
 	"context"
 	"errors"
 	"fmt"
-
 	catalog "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
 	"kubedb.dev/apimachinery/apis/kubedb"
 
-	core "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	ofst "kmodules.xyz/offshoot-api/api/v2"
+	olddbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
+// SetupClickHouseWebhookWithManager registers the webhook for ClickHouse in the manager.
+func SetupClickHouseWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&olddbapi.ClickHouse{}).
+		WithValidator(&ClickHouseCustomWebhook{mgr.GetClient()}).
+		WithDefaulter(&ClickHouseCustomWebhook{mgr.GetClient()}).
+		Complete()
+}
+
+//+kubebuilder:webhook:path=/mutate-singlestore-kubedb-com-v1alpha1-singlestore,mutating=true,failurePolicy=fail,sideEffects=None,groups=kubedb.com,resources=singlestores,verbs=create;update,versions=v1alpha1,name=msinglestore.kb.io,admissionReviewVersions={v1,v1beta1}
+
+// +kubebuilder:object:generate=false
+type ClickHouseCustomWebhook struct {
+	DefaultClient client.Client
+}
+
+var _ webhook.CustomDefaulter = &ClickHouseCustomWebhook{}
+
 // log is for logging in this package.
 var clickhouselog = logf.Log.WithName("clickhouse-resource")
 
-var _ webhook.CustomDefaulter = &ClickHouse{}
-
 // Default implements webhook.Defaulter so a webhook will be registered for the type
-func (c *ClickHouse) Default(ctx context.Context, obj runtime.Object) error {
-	if c == nil {
-		return nil
+func (c *ClickHouseCustomWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	db, ok := obj.(*olddbapi.ClickHouse)
+	if !ok {
+		return fmt.Errorf("expected an ClickHouse object but got %T", obj)
 	}
-	clickhouselog.Info("default", "name", c.Name)
-	c.SetDefaults()
+	clickhouselog.Info("default", "name", db.Name)
+	db.SetDefaults(c.DefaultClient)
 	return nil
 }
 
-var _ webhook.CustomValidator = &ClickHouse{}
+var _ webhook.CustomValidator = &ClickHouseCustomWebhook{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (c *ClickHouse) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	clickhouselog.Info("validate create", "name", c.Name)
-	return nil, c.ValidateCreateOrUpdate()
+func (c *ClickHouseCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	db, ok := obj.(*olddbapi.ClickHouse)
+	if !ok {
+		return nil, fmt.Errorf("expected an ClickHouse object but got %T", obj)
+	}
+	clickhouselog.Info("validate create", "name", db.Name)
+	return nil, c.ValidateCreateOrUpdate(db)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (c *ClickHouse) ValidateUpdate(ctx context.Context, old, newObj runtime.Object) (admission.Warnings, error) {
-	clickhouselog.Info("validate update", "name", c.Name)
-	return nil, c.ValidateCreateOrUpdate()
+func (c *ClickHouseCustomWebhook) ValidateUpdate(ctx context.Context, old, newObj runtime.Object) (admission.Warnings, error) {
+	db, ok := newObj.(*olddbapi.ClickHouse)
+	if !ok {
+		return nil, fmt.Errorf("expected an ClickHouse object but got %T", newObj)
+	}
+	clickhouselog.Info("validate update", "name", db.Name)
+	return nil, c.ValidateCreateOrUpdate(db)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
-func (c *ClickHouse) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	clickhouselog.Info("validate delete", "name", c.Name)
+func (c *ClickHouseCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	db, ok := obj.(*olddbapi.ClickHouse)
+	if !ok {
+		return nil, fmt.Errorf("expected an ClickHouse object but got %T", obj)
+	}
+	clickhouselog.Info("validate delete", "name", db.Name)
 
 	var allErr field.ErrorList
-	if c.Spec.DeletionPolicy == DeletionPolicyDoNotTerminate {
+	if db.Spec.DeletionPolicy == olddbapi.DeletionPolicyDoNotTerminate {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("teminationPolicy"),
-			c.Name,
+			db.Name,
 			"Can not delete as terminationPolicy is set to \"DoNotTerminate\""))
-		return nil, apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, c.Name, allErr)
+		return nil, apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
 	}
 	return nil, nil
 }
 
-func (c *ClickHouse) ValidateCreateOrUpdate() error {
+func (c *ClickHouseCustomWebhook) ValidateCreateOrUpdate(db *olddbapi.ClickHouse) error {
 	var allErr field.ErrorList
 
-	if c.Spec.Version == "" {
+	if db.Spec.Version == "" {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("version"),
-			c.Name,
+			db.Name,
 			"spec.version' is missing"))
-		return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, c.Name, allErr)
+		return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
 	} else {
-		err := c.ValidateVersion(c)
+		err := c.ValidateVersion(db)
 		if err != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("version"),
-				c.Spec.Version,
+				db.Spec.Version,
 				err.Error()))
-			return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, c.Name, allErr)
+			return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
 		}
 	}
 
-	if c.Spec.DisableSecurity {
-		if c.Spec.AuthSecret != nil {
+	if db.Spec.DisableSecurity {
+		if db.Spec.AuthSecret != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("authSecret"),
-				c.Name,
+				db.Name,
 				"authSecret should be nil when security is disabled"))
-			return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, c.Name, allErr)
+			return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
 		}
 	}
 
-	if c.Spec.ClusterTopology != nil {
+	if db.Spec.ClusterTopology != nil {
 		clusterName := map[string]bool{}
-		clusters := c.Spec.ClusterTopology.Cluster
-		if c.Spec.ClusterTopology.ClickHouseKeeper != nil {
-			if !c.Spec.ClusterTopology.ClickHouseKeeper.ExternallyManaged {
-				if c.Spec.ClusterTopology.ClickHouseKeeper.Spec == nil {
+		clusters := db.Spec.ClusterTopology.Cluster
+		if db.Spec.ClusterTopology.ClickHouseKeeper != nil {
+			if !db.Spec.ClusterTopology.ClickHouseKeeper.ExternallyManaged {
+				if db.Spec.ClusterTopology.ClickHouseKeeper.Spec == nil {
 					allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec"),
-						c.Name,
+						db.Name,
 						"spec can't be nil when externally managed is false"))
 				} else {
-					if *c.Spec.ClusterTopology.ClickHouseKeeper.Spec.Replicas < 1 {
+					if *db.Spec.ClusterTopology.ClickHouseKeeper.Spec.Replicas < 1 {
 						allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec").Child("replica"),
-							c.Name,
+							db.Name,
 							"number of replica can not be 0 or less"))
 					}
-					allErr = c.validateClickHouseKeeperStorageType(c.Spec.ClusterTopology.ClickHouseKeeper.Spec.StorageType, c.Spec.ClusterTopology.ClickHouseKeeper.Spec.Storage, allErr)
+					allErr = c.validateClickHouseKeeperStorageType(db, allErr)
 				}
-				if c.Spec.ClusterTopology.ClickHouseKeeper.Node != nil {
+				if db.Spec.ClusterTopology.ClickHouseKeeper.Node != nil {
 					allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("node"),
-						c.Name,
+						db.Name,
 						"ClickHouse Keeper node should be empty when externally managed is false"))
 				}
 			} else {
-				if c.Spec.ClusterTopology.ClickHouseKeeper.Node == nil {
+				if db.Spec.ClusterTopology.ClickHouseKeeper.Node == nil {
 					allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("node"),
-						c.Name,
+						db.Name,
 						"ClickHouse Keeper node can't be empty when externally managed is true"))
 				} else {
-					if c.Spec.ClusterTopology.ClickHouseKeeper.Node.Host == "" {
+					if db.Spec.ClusterTopology.ClickHouseKeeper.Node.Host == "" {
 						allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("node").Child("host"),
-							c.Name,
+							db.Name,
 							"ClickHouse Keeper host can't be empty"))
 					}
-					if c.Spec.ClusterTopology.ClickHouseKeeper.Node.Port == nil {
+					if db.Spec.ClusterTopology.ClickHouseKeeper.Node.Port == nil {
 						allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("node").Child("port"),
-							c.Name,
+							db.Name,
 							"ClickHouse Keeper port can't be empty"))
 					}
 				}
-				if c.Spec.ClusterTopology.ClickHouseKeeper.Spec != nil {
+				if db.Spec.ClusterTopology.ClickHouseKeeper.Spec != nil {
 					allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec"),
-						c.Name,
+						db.Name,
 						"ClickHouse Keeper spec should be empty when externally managed is true"))
 				}
 			}
@@ -155,162 +184,162 @@ func (c *ClickHouse) ValidateCreateOrUpdate() error {
 		for _, cluster := range clusters {
 			if cluster.Shards != nil && *cluster.Shards <= 0 {
 				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("shards"),
-					c.Name,
+					db.Name,
 					"number of shards can not be 0 or less"))
 			}
 			if cluster.Replicas != nil && *cluster.Replicas <= 0 {
 				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("replicas"),
-					c.Name,
+					db.Name,
 					"number of replicas can't be 0 or less"))
 			}
 			if clusterName[cluster.Name] {
 				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child(cluster.Name),
-					c.Name,
+					db.Name,
 					"cluster name is already exists, use different cluster name"))
 			}
 			clusterName[cluster.Name] = true
 
-			allErr = c.validateClusterStorageType(cluster, allErr)
+			allErr = c.validateClusterStorageType(db, cluster, allErr)
 
 			err := c.validateVolumes(cluster.PodTemplate)
 			if err != nil {
 				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("podTemplate").Child("spec").Child("volumes"),
-					c.Name,
+					db.Name,
 					err.Error()))
 			}
 			err = c.validateVolumesMountPaths(cluster.PodTemplate)
 			if err != nil {
 				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("podTemplate").Child("spec").Child("volumeMounts"),
-					c.Name,
+					db.Name,
 					err.Error()))
 			}
 		}
-		if c.Spec.PodTemplate != nil {
+		if db.Spec.PodTemplate != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("podTemplate"),
-				c.Name,
+				db.Name,
 				"PodTemplate should be nil in clusterTopology"))
 		}
 
-		if c.Spec.Replicas != nil {
+		if db.Spec.Replicas != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("replica"),
-				c.Name,
+				db.Name,
 				"replica should be nil in clusterTopology"))
 		}
 
-		if c.Spec.StorageType != "" {
+		if db.Spec.StorageType != "" {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storageType"),
-				c.Name,
+				db.Name,
 				"StorageType should be empty in clusterTopology"))
 		}
 
-		if c.Spec.Storage != nil {
+		if db.Spec.Storage != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storage"),
-				c.Name,
+				db.Name,
 				"storage should be nil in clusterTopology"))
 		}
 
 	} else {
 		// number of replicas can not be 0 or less
-		if c.Spec.Replicas != nil && *c.Spec.Replicas <= 0 {
+		if db.Spec.Replicas != nil && *db.Spec.Replicas <= 0 {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("replicas"),
-				c.Name,
+				db.Name,
 				"number of replicas can't be 0 or less"))
 		}
 
 		// number of replicas can not be greater than 1
-		if c.Spec.Replicas != nil && *c.Spec.Replicas > 1 {
+		if db.Spec.Replicas != nil && *db.Spec.Replicas > 1 {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("replicas"),
-				c.Name,
+				db.Name,
 				"number of replicas can't be greater than 1 in standalone mode"))
 		}
-		err := c.validateVolumes(c.Spec.PodTemplate)
+		err := c.validateVolumes(db.Spec.PodTemplate)
 		if err != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("podTemplate").Child("spec").Child("volumes"),
-				c.Name,
+				db.Name,
 				err.Error()))
 		}
-		err = c.validateVolumesMountPaths(c.Spec.PodTemplate)
+		err = c.validateVolumesMountPaths(db.Spec.PodTemplate)
 		if err != nil {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("podTemplate").Child("spec").Child("volumeMounts"),
-				c.Name,
+				db.Name,
 				err.Error()))
 		}
 
-		allErr = c.validateStandaloneStorageType(c.Spec.StorageType, c.Spec.Storage, allErr)
+		allErr = c.validateStandaloneStorageType(db, allErr)
 	}
 
 	if len(allErr) == 0 {
 		return nil
 	}
-	return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, c.Name, allErr)
+	return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
 }
 
-func (c *ClickHouse) validateStandaloneStorageType(storageType StorageType, storage *core.PersistentVolumeClaimSpec, allErr field.ErrorList) field.ErrorList {
-	if storageType == "" {
+func (c *ClickHouseCustomWebhook) validateStandaloneStorageType(db *olddbapi.ClickHouse, allErr field.ErrorList) field.ErrorList {
+	if db.Spec.StorageType == "" {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storageType"),
-			c.Name,
+			db.Name,
 			"StorageType can not be empty"))
 	} else {
-		if storageType != StorageTypeDurable && c.Spec.StorageType != StorageTypeEphemeral {
+		if db.Spec.StorageType != olddbapi.StorageTypeDurable && db.Spec.StorageType != olddbapi.StorageTypeEphemeral {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storageType"),
-				c.Name,
+				db.Name,
 				"StorageType should be either durable or ephemeral"))
 		}
 	}
 
-	if storage == nil && c.Spec.StorageType == StorageTypeDurable {
+	if db.Spec.Storage == nil && db.Spec.StorageType == olddbapi.StorageTypeDurable {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storage"),
-			c.Name,
+			db.Name,
 			"Storage can't be empty when StorageType is durable"))
 	}
 
 	return allErr
 }
 
-func (c *ClickHouse) validateClusterStorageType(cluster ClusterSpec, allErr field.ErrorList) field.ErrorList {
+func (c *ClickHouseCustomWebhook) validateClusterStorageType(db *olddbapi.ClickHouse, cluster olddbapi.ClusterSpec, allErr field.ErrorList) field.ErrorList {
 	if cluster.StorageType == "" {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child(cluster.Name).Child("storageType"),
-			c.Name,
+			db.Name,
 			"StorageType can not be empty"))
 	} else {
-		if cluster.StorageType != StorageTypeDurable && cluster.StorageType != StorageTypeEphemeral {
+		if cluster.StorageType != olddbapi.StorageTypeDurable && cluster.StorageType != olddbapi.StorageTypeEphemeral {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child(cluster.Name).Child("storageType"),
 				cluster.StorageType,
 				"StorageType should be either durable or ephemeral"))
 		}
 	}
-	if cluster.Storage == nil && cluster.StorageType == StorageTypeDurable {
+	if cluster.Storage == nil && cluster.StorageType == olddbapi.StorageTypeDurable {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child(cluster.Name).Child("storage"),
-			c.Name,
+			db.Name,
 			"Storage can't be empty when StorageType is durable"))
 	}
 	return allErr
 }
 
-func (c *ClickHouse) validateClickHouseKeeperStorageType(storageType StorageType, storage *core.PersistentVolumeClaimSpec, allErr field.ErrorList) field.ErrorList {
-	if storageType == "" {
+func (c *ClickHouseCustomWebhook) validateClickHouseKeeperStorageType(db *olddbapi.ClickHouse, allErr field.ErrorList) field.ErrorList {
+	if db.Spec.ClusterTopology.ClickHouseKeeper.Spec.StorageType == "" {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec").Child("storageType"),
-			c.Name,
+			db.Name,
 			"StorageType can not be empty"))
 	} else {
-		if storageType != StorageTypeDurable && c.Spec.StorageType != StorageTypeEphemeral {
+		if db.Spec.ClusterTopology.ClickHouseKeeper.Spec.StorageType != olddbapi.StorageTypeDurable && db.Spec.StorageType != olddbapi.StorageTypeEphemeral {
 			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec").Child("storageType"),
-				c.Name,
+				db.Name,
 				"StorageType should be either durable or ephemeral"))
 		}
 	}
-	if storage == nil && c.Spec.StorageType == StorageTypeDurable {
+	if db.Spec.ClusterTopology.ClickHouseKeeper.Spec.Storage == nil && db.Spec.StorageType == olddbapi.StorageTypeDurable {
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("clusterTopology").Child("clickHouseKeeper").Child("spec").Child("storage"),
-			c.Name,
+			db.Name,
 			"Storage can't be empty when StorageType is durable"))
 	}
 
 	return allErr
 }
 
-func (c *ClickHouse) ValidateVersion(db *ClickHouse) error {
+func (c *ClickHouseCustomWebhook) ValidateVersion(db *olddbapi.ClickHouse) error {
 	chVersion := catalog.ClickHouseVersion{}
-	err := DefaultClient.Get(context.TODO(), types.NamespacedName{Name: db.Spec.Version}, &chVersion)
+	err := c.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: db.Spec.Version}, &chVersion)
 	if err != nil {
 		// fmt.Sprint(db.Spec.Version, "version not supported")
 		return errors.New(fmt.Sprint("version ", db.Spec.Version, " not supported"))
@@ -322,7 +351,7 @@ var clickhouseReservedVolumes = []string{
 	kubedb.ClickHouseVolumeData,
 }
 
-func (c *ClickHouse) validateVolumes(podTemplate *ofst.PodTemplateSpec) error {
+func (c *ClickHouseCustomWebhook) validateVolumes(podTemplate *ofst.PodTemplateSpec) error {
 	if podTemplate.Spec.Volumes == nil {
 		return nil
 	}
@@ -343,7 +372,7 @@ var clickhouseReservedVolumeMountPaths = []string{
 	kubedb.ClickHouseDataDir,
 }
 
-func (c *ClickHouse) validateVolumesMountPaths(podTemplate *ofst.PodTemplateSpec) error {
+func (c *ClickHouseCustomWebhook) validateVolumesMountPaths(podTemplate *ofst.PodTemplateSpec) error {
 	if podTemplate == nil {
 		return nil
 	}
