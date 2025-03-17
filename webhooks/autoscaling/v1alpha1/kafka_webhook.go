56981b81271c9e56e05a4e314ccfcadad33e5cd5
obaydullahmhs <obaydullah@appscode.com>
2025-03-17 15:16:04 +0600

    Add Kafka autoscaler webhook
    Signed-off-by: obaydullahmhs <obaydullah@appscode.com>

--- webhooks/autoscaling/v1alpha1/kafka_webhook.go
+++ webhooks/autoscaling/v1alpha1/kafka_webhook.go
@@ -27,6 +27,8 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -35,92 +37,120 @@ import (
 // log is for logging in this package.
 var kafkaLog = logf.Log.WithName("kafka-autoscaler")
 
-var _ webhook.CustomDefaulter = &autoscalingapi.KafkaAutoscaler{}
+// SetupKafkaAutoscalerWebhookWithManager registers the webhook for KafkaAutoscaler in the manager.
+func SetupKafkaAutoscalerWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&autoscalingapi.KafkaAutoscaler{}).
+		WithValidator(&KafkaAutoscalerCustomWebhook{mgr.GetClient()}).
+		WithDefaulter(&KafkaAutoscalerCustomWebhook{mgr.GetClient()}).
+		Complete()
+}
+
+type KafkaAutoscalerCustomWebhook struct {
+	DefaultClient client.Client
+}
+
+var _ webhook.CustomDefaulter = &KafkaAutoscalerCustomWebhook{}
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the type
-func (k *autoscalingapi.KafkaAutoscaler) Default(ctx context.Context, obj runtime.Object) error {
-	kafkaLog.Info("defaulting", "name", k.Name)
-	k.setDefaults()
+func (k *KafkaAutoscalerCustomWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	scaler, ok := obj.(*autoscalingapi.KafkaAutoscaler)
+	if !ok {
+		return fmt.Errorf("expected an KafkaAutoscaler object but got %T", obj)
+	}
+	kafkaLog.Info("defaulting", "name", scaler.Name)
+	k.setDefaults(scaler)
 	return nil
 }
 
-func (k *autoscalingapi.KafkaAutoscaler) setDefaults() {
+func (k *KafkaAutoscalerCustomWebhook) setDefaults(scaler *autoscalingapi.KafkaAutoscaler) {
 	var db dbapi.Kafka
-	err := autoscalingapi.DefaultClient.Get(context.TODO(), types.NamespacedName{
-		Name:      k.Spec.DatabaseRef.Name,
-		Namespace: k.Namespace,
+	err := k.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name:      scaler.Spec.DatabaseRef.Name,
+		Namespace: scaler.Namespace,
 	}, &db)
 	if err != nil {
-		_ = fmt.Errorf("can't get Kafka %s/%s \n", k.Namespace, k.Spec.DatabaseRef.Name)
+		_ = fmt.Errorf("can't get Kafka %s/%s \n", scaler.Namespace, scaler.Spec.DatabaseRef.Name)
 		return
 	}
 
-	k.setOpsReqOptsDefaults()
+	k.setOpsReqOptsDefaults(scaler)
 
-	if k.Spec.Storage != nil {
+	if scaler.Spec.Storage != nil {
 		if db.Spec.Topology != nil {
-			setDefaultStorageValues(k.Spec.Storage.Broker)
-			setDefaultStorageValues(k.Spec.Storage.Controller)
+			setDefaultStorageValues(scaler.Spec.Storage.Broker)
+			setDefaultStorageValues(scaler.Spec.Storage.Controller)
 		} else {
-			setDefaultStorageValues(k.Spec.Storage.Node)
+			setDefaultStorageValues(scaler.Spec.Storage.Node)
 		}
 	}
 
-	if k.Spec.Compute != nil {
+	if scaler.Spec.Compute != nil {
 		if db.Spec.Topology != nil {
-			setDefaultComputeValues(k.Spec.Compute.Broker)
-			setDefaultComputeValues(k.Spec.Compute.Controller)
+			setDefaultComputeValues(scaler.Spec.Compute.Broker)
+			setDefaultComputeValues(scaler.Spec.Compute.Controller)
 		} else {
-			setDefaultComputeValues(k.Spec.Compute.Node)
+			setDefaultComputeValues(scaler.Spec.Compute.Node)
 		}
 	}
 }
 
-func (k *autoscalingapi.KafkaAutoscaler) setOpsReqOptsDefaults() {
-	if k.Spec.OpsRequestOptions == nil {
-		k.Spec.OpsRequestOptions = &autoscalingapi.KafkaOpsRequestOptions{}
+func (k *KafkaAutoscalerCustomWebhook) setOpsReqOptsDefaults(scaler *autoscalingapi.KafkaAutoscaler) {
+	if scaler.Spec.OpsRequestOptions == nil {
+		scaler.Spec.OpsRequestOptions = &autoscalingapi.KafkaOpsRequestOptions{}
 	}
 	// Timeout is defaulted to 600s in ops-manager retries.go (to retry 120 times with 5sec pause between each)
 	// OplogMaxLagSeconds & ObjectsCountDiffPercentage are defaults to 0
-	if k.Spec.OpsRequestOptions.Apply == "" {
-		k.Spec.OpsRequestOptions.Apply = opsapi.ApplyOptionIfReady
+	if scaler.Spec.OpsRequestOptions.Apply == "" {
+		scaler.Spec.OpsRequestOptions.Apply = opsapi.ApplyOptionIfReady
 	}
 }
 
-var _ webhook.CustomValidator = &autoscalingapi.KafkaAutoscaler{}
+var _ webhook.CustomValidator = &KafkaAutoscalerCustomWebhook{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
-func (k *autoscalingapi.KafkaAutoscaler) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	kafkaLog.Info("validate create", "name", k.Name)
-	return nil, k.validate()
+func (k *KafkaAutoscalerCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	scaler, ok := obj.(*autoscalingapi.KafkaAutoscaler)
+	if !ok {
+		return nil, fmt.Errorf("expected an KafkaAutoscaler object but got %T", obj)
+	}
+	kafkaLog.Info("validate create", "name", scaler.Name)
+	return nil, k.validate(scaler)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
-func (k *autoscalingapi.KafkaAutoscaler) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
-	kafkaLog.Info("validate create", "name", k.Name)
-	return nil, k.validate()
+func (k *KafkaAutoscalerCustomWebhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	scaler, ok := newObj.(*autoscalingapi.KafkaAutoscaler)
+	if !ok {
+		return nil, fmt.Errorf("expected an KafkaAutoscaler object but got %T", newObj)
+	}
+	kafkaLog.Info("validate create", "name", scaler.Name)
+	return nil, k.validate(scaler)
 }
 
-func (_ *autoscalingapi.KafkaAutoscaler) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+func (_ *KafkaAutoscalerCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	_, ok := obj.(*autoscalingapi.KafkaAutoscaler)
+	if !ok {
+		return nil, fmt.Errorf("expected an KafkaAutoscaler object but got %T", obj)
+	}
 	return nil, nil
 }
 
-func (k *autoscalingapi.KafkaAutoscaler) validate() error {
-	if k.Spec.DatabaseRef == nil {
+func (k *KafkaAutoscalerCustomWebhook) validate(scaler *autoscalingapi.KafkaAutoscaler) error {
+	if scaler.Spec.DatabaseRef == nil {
 		return errors.New("databaseRef can't be empty")
 	}
 	var kf dbapi.Kafka
 	err := autoscalingapi.DefaultClient.Get(context.TODO(), types.NamespacedName{
-		Name:      k.Spec.DatabaseRef.Name,
-		Namespace: k.Namespace,
+		Name:      scaler.Spec.DatabaseRef.Name,
+		Namespace: scaler.Namespace,
 	}, &kf)
 	if err != nil {
-		_ = fmt.Errorf("can't get Kafka %s/%s \n", k.Namespace, k.Spec.DatabaseRef.Name)
+		_ = fmt.Errorf("can't get Kafka %s/%s \n", scaler.Namespace, scaler.Spec.DatabaseRef.Name)
 		return err
 	}
 
-	if k.Spec.Compute != nil {
-		cm := k.Spec.Compute
+	if scaler.Spec.Compute != nil {
+		cm := scaler.Spec.Compute
 		if kf.Spec.Topology != nil {
 			if cm.Node != nil {
 				return errors.New("Spec.Compute.Node is invalid for kafka with topology")
@@ -135,8 +165,8 @@ func (k *autoscalingapi.KafkaAutoscaler) validate() error {
 		}
 	}
 
-	if k.Spec.Storage != nil {
-		st := k.Spec.Storage
+	if scaler.Spec.Storage != nil {
+		st := scaler.Spec.Storage
 		if kf.Spec.Topology != nil {
 			if st.Node != nil {
 				return errors.New("Spec.Storage.Node is invalid for kafka with topology")
