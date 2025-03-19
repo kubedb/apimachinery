2bb12f870d9a3b14f1151722e36736115a60d048
Neaj Morshad <neaj@appscode.com>
2025-03-19 11:49:10 +0600

    Move & Convert Redis & Redis Sentinell
    Signed-off-by: Neaj Morshad <neaj@appscode.com>

---
+++ pkg/webhooks/kubedb/v1/redis.go
@@ -0,0 +1,373 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	catalogapi "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
+	"kubedb.dev/apimachinery/apis/kubedb"
+	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1"
+	olddbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
+	amv "kubedb.dev/apimachinery/pkg/validator"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/pkg/errors"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/mergepatch"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/utils/ptr"
+	meta_util "kmodules.xyz/client-go/meta"
+	ofstv1 "kmodules.xyz/offshoot-api/api/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// SetupRedisWebhookWithManager registers the webhook for Redis in the manager.
+func SetupRedisWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&dbapi.Redis{}).
+		WithValidator(&RedisCustomWebhook{mgr.GetClient(), true}).
+		WithDefaulter(&RedisCustomWebhook{mgr.GetClient(), true}).
+		Complete()
+}
+
+type RedisCustomWebhook struct {
+	DefaultClient    client.Client
+	StrictValidation bool
+}
+
+var _ webhook.CustomDefaulter = &RedisCustomWebhook{}
+
+func (w RedisCustomWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	log := logf.FromContext(ctx)
+	log.Info("defaulting Redis")
+
+	redis := obj.(*dbapi.Redis)
+	if redis.Spec.Version == "" {
+		return errors.New(`'spec.version' is missing`)
+	}
+
+	if redis.Spec.Halted {
+		if redis.Spec.DeletionPolicy == dbapi.DeletionPolicyDoNotTerminate {
+			return errors.New(`Can't halt, since termination policy is 'DoNotTerminate'`)
+		}
+		redis.Spec.DeletionPolicy = dbapi.DeletionPolicyHalt
+	}
+
+	var redisVersion catalogapi.RedisVersion
+	err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: redis.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get RedisVersion: %s", redis.Spec.Version)
+	}
+
+	return redis.SetDefaults(&redisVersion)
+}
+
+var _ webhook.CustomValidator = &RedisCustomWebhook{}
+
+func (w RedisCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	return w.validate(ctx, obj.(*dbapi.Redis))
+}
+
+func (w RedisCustomWebhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error) {
+	oldRedis, ok := oldObj.(*dbapi.Redis)
+	if !ok {
+		return nil, fmt.Errorf("expected a Redis but got a %T", oldRedis)
+	}
+	redis, ok := newObj.(*dbapi.Redis)
+	if !ok {
+		return nil, fmt.Errorf("expected a Redis but got a %T", redis)
+	}
+
+	var redisVersion catalogapi.RedisVersion
+	err = w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: oldRedis.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get redisversion: %s", oldRedis.Spec.Version)
+	}
+
+	err = oldRedis.SetDefaults(&redisVersion)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to set defaults for RedisVersion: %s", oldRedis.Spec.Version)
+	}
+
+	if oldRedis.Spec.AuthSecret == nil {
+		oldRedis.Spec.AuthSecret = redis.Spec.AuthSecret
+	}
+	if err := validateRedisUpdate(redis, oldRedis); err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+	return w.validate(ctx, redis)
+}
+
+func (w RedisCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	redis, ok := obj.(*dbapi.Redis)
+	if !ok {
+		return nil, fmt.Errorf("expected a Redis but got a %T", obj)
+	}
+
+	var rd dbapi.Redis
+	err = w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name:      redis.Name,
+		Namespace: redis.Namespace,
+	}, &rd)
+	if err != nil && !kerr.IsNotFound(err) {
+		return nil, errors.Wrapf(err, "failed to get Redis: %s", redis.Name)
+	} else if err == nil && rd.Spec.DeletionPolicy == dbapi.DeletionPolicyDoNotTerminate {
+		return nil, fmt.Errorf(`redis "%v/%v" can't be terminated. To delete, change spec.deletionPolicy`, rd.Namespace, rd.Name)
+	}
+	return nil, nil
+}
+
+var forbiddenRedisEnvVars = []string{
+	"REDISCLI_AUTH",
+	"SENTINEL_PASSWORD",
+}
+
+var redisReservedVolumes = []string{
+	kubedb.RedisDataVolumeName,
+	kubedb.RedisScriptVolumeName,
+	kubedb.RedisTLSVolumeName,
+	kubedb.RedisExporterTLSVolumeName,
+	kubedb.RedisConfigVolumeName,
+}
+
+var redisReservedMountPaths = []string{
+	kubedb.RedisDataVolumePath,
+	kubedb.RedisScriptVolumePath,
+	kubedb.RedisTLSVolumePath,
+	kubedb.RedisConfigVolumePath,
+}
+
+func validateRedisUpdate(obj, oldObj *dbapi.Redis) error {
+	preconditions := meta_util.PreConditionSet{
+		Set: sets.New[string](
+			"spec.storageType",
+			"spec.podTemplate.spec.nodeSelector",
+		),
+	}
+	// Once the database has been initialized, don't let update the "spec.init" section
+	if oldObj.Spec.Init != nil && oldObj.Spec.Init.Initialized {
+		preconditions.Insert("spec.init")
+	}
+	_, err := meta_util.CreateStrategicPatch(oldObj, obj, preconditions.PreconditionFunc()...)
+	if err != nil {
+		if mergepatch.IsPreconditionFailed(err) {
+			return fmt.Errorf("%v.%v", err, preconditions.Error())
+		}
+		return err
+	}
+	return nil
+}
+
+func validateRedisEnvsForAllContainers(redis *dbapi.Redis) error {
+	var err error
+	for _, container := range redis.Spec.PodTemplate.Spec.Containers {
+		if errC := amv.ValidateEnvVar(container.Env, forbiddenEnvVars, dbapi.ResourceKindRedis); errC != nil {
+			if err == nil {
+				err = errC
+			} else {
+				err = errors.Wrap(err, errC.Error())
+			}
+		}
+	}
+	return err
+}
+
+func validateRedisVolumeMountsForAllContainers(redis *dbapi.Redis) error {
+	var err error
+	for _, container := range redis.Spec.PodTemplate.Spec.Containers {
+		if errC := amv.ValidateMountPaths(container.VolumeMounts, reservedMountPaths); errC != nil {
+			if err == nil {
+				err = errC
+			} else {
+				err = errors.Wrap(err, errC.Error())
+			}
+		}
+	}
+	return err
+}
+
+func checkTLSSupport(v string) error {
+	rdVersion, err := semver.NewVersion(v)
+	if err != nil {
+		return err
+	}
+	if rdVersion.Major() < 6 {
+		return fmt.Errorf("ssl support is available only for v6 or later versions")
+	}
+	return nil
+}
+
+func (w RedisCustomWebhook) validate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+	redis, ok := obj.(*dbapi.Redis)
+	if !ok {
+		return nil, fmt.Errorf("expected a Redis but got a %T", obj)
+	}
+
+	if redis.Spec.Version == "" {
+		return nil, errors.New(`'spec.version' is missing`)
+	}
+	var redisVersion catalogapi.RedisVersion
+	err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: redis.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	if redis.Spec.Mode != dbapi.RedisModeStandalone && redis.Spec.Mode != dbapi.RedisModeCluster && redis.Spec.Mode != dbapi.RedisModeSentinel {
+		return nil, fmt.Errorf(`spec.mode "%v" invalid. Value must be one of "%v", "%v" or "%v"`,
+			redis.Spec.Mode, dbapi.RedisModeStandalone, dbapi.RedisModeCluster, dbapi.RedisModeSentinel)
+	}
+
+	if redis.Spec.Mode == dbapi.RedisModeStandalone && ptr.Deref(redis.Spec.Replicas, 0) != 1 {
+		return nil, fmt.Errorf(`spec.replicas "%d" invalid for standalone mode. Value must be one`, ptr.Deref(redis.Spec.Replicas, 0))
+	}
+
+	if redis.Spec.Mode == dbapi.RedisModeCluster && ptr.Deref(redis.Spec.Cluster.Shards, 0) < 3 {
+		return nil, fmt.Errorf(`spec.cluster.shards "%d" invalid. Value must be >= 3`, ptr.Deref(redis.Spec.Cluster.Shards, 0))
+	}
+
+	if redis.Spec.Mode == dbapi.RedisModeCluster && ptr.Deref(redis.Spec.Cluster.Replicas, 0) < 2 {
+		return nil, fmt.Errorf(`spec.cluster.replicas "%d" invalid. Value must be > 1`, ptr.Deref(redis.Spec.Cluster.Replicas, 0))
+	}
+	if redis.Spec.Mode == dbapi.RedisModeSentinel && (redis.Spec.SentinelRef == nil || redis.Spec.SentinelRef.Name == "" || redis.Spec.SentinelRef.Namespace == "") {
+		return nil, fmt.Errorf("need to provide sentinelRef Name and Namespace while redis Mode set to Sentinel for redis %s/%s", redis.Namespace, redis.Name)
+	}
+
+	if redis.Spec.StorageType == "" {
+		return nil, fmt.Errorf(`'spec.storageType' is missing`)
+	}
+	if redis.Spec.StorageType != dbapi.StorageTypeDurable && redis.Spec.StorageType != dbapi.StorageTypeEphemeral {
+		return nil, fmt.Errorf(`'spec.storageType' %s is invalid`, redis.Spec.StorageType)
+	}
+	if err := amv.ValidateStorage(w.DefaultClient, olddbapi.StorageType(redis.Spec.StorageType), redis.Spec.Storage); err != nil {
+		return nil, err
+	}
+	err = amv.ValidateVolumes(ofstv1.ConvertVolumes(redis.Spec.PodTemplate.Spec.Volumes), reservedVolumes)
+	if err != nil {
+		return nil, err
+	}
+	err = validateRedisVolumeMountsForAllContainers(redis)
+	if err != nil {
+		return nil, err
+	}
+	err = amv.ValidateHealth(&redis.Spec.HealthChecker)
+	if err != nil {
+		return nil, err
+	}
+
+	if redis.Spec.DisableAuth && redis.Spec.AuthSecret != nil {
+		return nil, fmt.Errorf("auth Secret is not supported when disableAuth is true")
+	}
+	if redis.Spec.Mode == dbapi.RedisModeSentinel {
+		err = validateSentinelVersion(redisVersion.Spec.Version)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// if secret managed externally verify auth secret name is not empty
+	if !redis.Spec.DisableAuth {
+		if redis.Spec.AuthSecret != nil &&
+			redis.Spec.AuthSecret.ExternallyManaged &&
+			redis.Spec.AuthSecret.Name == "" {
+			return nil, fmt.Errorf("for externallyManaged auth secret, user need to provide \"redis.Spec.AuthSecret.Name\"")
+		}
+	}
+
+	if w.StrictValidation {
+		// Check if redisVersion is deprecated.
+		// If deprecated, return error
+		if redisVersion.Spec.Deprecated {
+			return nil, fmt.Errorf("redis %s/%s is using deprecated version %v. Skipped processing",
+				redis.Namespace, redis.Name, redisVersion.Name)
+		}
+
+		if err := redisVersion.ValidateSpecs(); err != nil {
+			return nil, fmt.Errorf("redis %s/%s is using invalid redisVersion %v. Skipped processing. reason: %v", redis.Namespace,
+				redis.Name, redisVersion.Name, err)
+		}
+	}
+
+	if redis.Spec.DeletionPolicy == "" {
+		return nil, fmt.Errorf(`'spec.DeletionPolicy' is missing`)
+	}
+
+	if redis.Spec.StorageType == dbapi.StorageTypeEphemeral && redis.Spec.DeletionPolicy == dbapi.DeletionPolicyHalt {
+		return nil, fmt.Errorf(`'spec.DeletionPolicy: Halt' can not be used for 'Ephemeral' storage`)
+	}
+
+	if redis.Spec.TLS != nil {
+		if err := checkTLSSupport(redisVersion.Spec.Version); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := validateRedisEnvsForAllContainers(redis); err != nil {
+		return nil, err
+	}
+
+	monitorSpec := redis.Spec.Monitor
+	if monitorSpec != nil {
+		if err := amv.ValidateMonitorSpec(monitorSpec); err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
+
+func ValidateForSentinel(kbClient client.Client, redis *dbapi.Redis) error {
+	if redis.Spec.Mode != dbapi.RedisModeSentinel {
+		return nil
+	}
+	if redis.Spec.SentinelRef == nil || redis.Spec.SentinelRef.Name == "" || redis.Spec.SentinelRef.Namespace == "" {
+		return fmt.Errorf("need to provide sentinelRef Name and Namespace while redis Mode set to Sentinel for redis %s/%s", redis.Namespace, redis.Name)
+	}
+
+	var sentinelDB dbapi.RedisSentinel
+	err := kbClient.Get(context.TODO(), types.NamespacedName{
+		Name:      redis.Spec.SentinelRef.Name,
+		Namespace: redis.Spec.SentinelRef.Namespace,
+	}, &sentinelDB)
+	if err != nil {
+		return err
+	}
+	if redis.Spec.TLS == nil && sentinelDB.Spec.TLS != nil {
+		return fmt.Errorf("can not start monitoring with TLS enabled Sentinel as Redis is not TLS enabled")
+	}
+	if redis.Spec.TLS != nil && sentinelDB.Spec.TLS == nil {
+		return fmt.Errorf("can not start monitoring with TLS disabled Sentinel as Redis is TLS is enabled")
+	}
+	if redis.Spec.TLS != nil {
+		if redis.Spec.TLS.IssuerRef.Kind != sentinelDB.Spec.TLS.IssuerRef.Kind ||
+			redis.Spec.TLS.IssuerRef.Name != sentinelDB.Spec.TLS.IssuerRef.Name {
+			return fmt.Errorf("can not use different issuer for redis and sentinel")
+		}
+	}
+	return nil
+}
---
+++ pkg/webhooks/kubedb/v1/redissentinel.go
@@ -0,0 +1,299 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	catalogapi "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
+	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1"
+	olddbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
+	amv "kubedb.dev/apimachinery/pkg/validator"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/pkg/errors"
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/mergepatch"
+	"k8s.io/apimachinery/pkg/util/sets"
+	meta_util "kmodules.xyz/client-go/meta"
+	ofstv1 "kmodules.xyz/offshoot-api/api/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// SetupRedisSentinelWebhookWithManager registers the webhook for RedisSentinel in the manager.
+func SetupRedisSentinelWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&dbapi.RedisSentinel{}).
+		WithValidator(&RedisSentinelCustomWebhook{mgr.GetClient(), true}).
+		WithDefaulter(&RedisSentinelCustomWebhook{mgr.GetClient(), true}).
+		Complete()
+}
+
+type RedisSentinelCustomWebhook struct {
+	DefaultClient    client.Client
+	StrictValidation bool
+}
+
+var _ webhook.CustomDefaulter = &RedisSentinelCustomWebhook{}
+
+func (w RedisSentinelCustomWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	log := logf.FromContext(ctx)
+	log.Info("defaulting RedisSentinel")
+
+	sentinel := obj.(*dbapi.RedisSentinel)
+	if sentinel.Spec.Version == "" {
+		return errors.New(`'spec.version' is missing`)
+	}
+
+	if sentinel.Spec.Halted {
+		if sentinel.Spec.DeletionPolicy == dbapi.DeletionPolicyDoNotTerminate {
+			return errors.New(`Can't halt, since termination policy is 'DoNotTerminate'`)
+		}
+		sentinel.Spec.DeletionPolicy = dbapi.DeletionPolicyHalt
+	}
+
+	var redisVersion catalogapi.RedisVersion
+	err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: sentinel.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return errors.Wrapf(err, "failed to get RedisVersion: %s", sentinel.Spec.Version)
+	}
+
+	return sentinel.SetDefaults(&redisVersion)
+}
+
+var _ webhook.CustomValidator = &RedisSentinelCustomWebhook{}
+
+func (w RedisSentinelCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	return w.validate(ctx, obj.(*dbapi.RedisSentinel))
+}
+
+func (w RedisSentinelCustomWebhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (warnings admission.Warnings, err error) {
+	oldRedisSentinel, ok := oldObj.(*dbapi.RedisSentinel)
+	if !ok {
+		return nil, fmt.Errorf("expected a RedisSentinel but got a %T", oldRedisSentinel)
+	}
+	sentinel, ok := newObj.(*dbapi.RedisSentinel)
+	if !ok {
+		return nil, fmt.Errorf("expected a RedisSentinel but got a %T", sentinel)
+	}
+
+	var redisVersion catalogapi.RedisVersion
+	err = w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: oldRedisSentinel.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get redisversion: %s", oldRedisSentinel.Spec.Version)
+	}
+
+	err = oldRedisSentinel.SetDefaults(&redisVersion)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to set defaults for RedisVersion: %s", oldRedisSentinel.Spec.Version)
+	}
+
+	if oldRedisSentinel.Spec.AuthSecret == nil {
+		oldRedisSentinel.Spec.AuthSecret = sentinel.Spec.AuthSecret
+	}
+	if err := validateSentinelUpdate(sentinel, oldRedisSentinel); err != nil {
+		return nil, fmt.Errorf("%v", err)
+	}
+	return w.validate(ctx, sentinel)
+}
+
+func (w RedisSentinelCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (warnings admission.Warnings, err error) {
+	sentinel, ok := obj.(*dbapi.RedisSentinel)
+	if !ok {
+		return nil, fmt.Errorf("expected a RedisSentinel but got a %T", obj)
+	}
+
+	var rs dbapi.RedisSentinel
+	err = w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name:      sentinel.Name,
+		Namespace: sentinel.Namespace,
+	}, &rs)
+	if err != nil && !kerr.IsNotFound(err) {
+		return nil, errors.Wrapf(err, "failed to get RedisSentinel: %s", sentinel.Name)
+	} else if err == nil && rs.Spec.DeletionPolicy == dbapi.DeletionPolicyDoNotTerminate {
+		return nil, fmt.Errorf(`sentinel "%v/%v" can't be terminated. To delete, change spec.deletionPolicy`, rs.Namespace, rs.Name)
+	}
+	return nil, nil
+}
+
+func validateSentinelUpdate(obj, oldObj *dbapi.RedisSentinel) error {
+	preconditions := meta_util.PreConditionSet{
+		Set: sets.New[string](
+			"spec.storageType",
+			"spec.storage",
+			"spec.podTemplate.spec.nodeSelector",
+		),
+	}
+
+	_, err := meta_util.CreateStrategicPatch(oldObj, obj, preconditions.PreconditionFunc()...)
+	if err != nil {
+		if mergepatch.IsPreconditionFailed(err) {
+			return fmt.Errorf("%v.%v", err, preconditions.Error())
+		}
+		return err
+	}
+	return nil
+}
+
+func (w RedisSentinelCustomWebhook) validate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+	sentinel, ok := obj.(*dbapi.RedisSentinel)
+	if !ok {
+		return nil, fmt.Errorf("expected a RedisSentinel but got a %T", obj)
+	}
+
+	if sentinel.Spec.Version == "" {
+		return nil, errors.New(`'spec.version' is missing`)
+	}
+
+	var redisVersion catalogapi.RedisVersion
+	err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name: sentinel.Spec.Version,
+	}, &redisVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	if sentinel.Spec.StorageType == "" {
+		return nil, fmt.Errorf(`'spec.storageType' is missing`)
+	}
+	if sentinel.Spec.StorageType != dbapi.StorageTypeDurable && sentinel.Spec.StorageType != dbapi.StorageTypeEphemeral {
+		return nil, fmt.Errorf(`'spec.storageType' %s is invalid`, sentinel.Spec.StorageType)
+	}
+	if err := amv.ValidateStorage(w.DefaultClient, olddbapi.StorageType(sentinel.Spec.StorageType), sentinel.Spec.Storage); err != nil {
+		return nil, err
+	}
+
+	err = amv.ValidateVolumes(ofstv1.ConvertVolumes(sentinel.Spec.PodTemplate.Spec.Volumes), reservedVolumes)
+	if err != nil {
+		return nil, err
+	}
+	// err = amv.ValidateMountPaths(sentinel.Spec.PodTemplate.Spec.VolumeMounts, reservedMountPaths)
+	err = validateSentinelVolumeMountsForAllContainers(sentinel)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validateSentinelVersion(redisVersion.Spec.Version)
+	if err != nil {
+		return nil, err
+	}
+	if sentinel.Spec.DisableAuth && sentinel.Spec.AuthSecret != nil {
+		return nil, fmt.Errorf("auth Secret is not supported when disableAuth is true")
+	}
+	err = amv.ValidateHealth(&sentinel.Spec.HealthChecker)
+	if err != nil {
+		return nil, err
+	}
+
+	if w.StrictValidation {
+		// Check if redisVersion is deprecated.
+		// If deprecated, return error
+		if redisVersion.Spec.Deprecated {
+			return nil, fmt.Errorf("redis Sentinel %s/%s is using deprecated version %v. Skipped processing",
+				sentinel.Namespace, sentinel.Name, redisVersion.Name)
+		}
+
+		if err := redisVersion.ValidateSpecs(); err != nil {
+			return nil, fmt.Errorf("redis Sentinel %s/%s is using invalid redisVersion %v. Skipped processing. reason: %v", sentinel.Namespace,
+				sentinel.Name, redisVersion.Name, err)
+		}
+	}
+
+	if !sentinel.Spec.DisableAuth {
+		if sentinel.Spec.AuthSecret != nil &&
+			sentinel.Spec.AuthSecret.ExternallyManaged &&
+			sentinel.Spec.AuthSecret.Name == "" {
+			return nil, fmt.Errorf("for externallyManaged auth secret, user need to provide \"redis.Spec.AuthSecret.Name\"")
+		}
+	}
+
+	if sentinel.Spec.DeletionPolicy == "" {
+		return nil, fmt.Errorf(`'spec.DeletionPolicy' is missing`)
+	}
+
+	if sentinel.Spec.StorageType == dbapi.StorageTypeEphemeral && sentinel.Spec.DeletionPolicy == dbapi.DeletionPolicyHalt {
+		return nil, fmt.Errorf(`'spec.DeletionPolicy: Halt' can not be used for 'Ephemeral' storage`)
+	}
+
+	if sentinel.Spec.TLS != nil {
+		if err := checkTLSSupport(redisVersion.Spec.Version); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := validateSentinelEnvsForAllContainers(sentinel); err != nil {
+		return nil, err
+	}
+
+	monitorSpec := sentinel.Spec.Monitor
+	if monitorSpec != nil {
+		if err := amv.ValidateMonitorSpec(monitorSpec); err != nil {
+			return nil, err
+		}
+	}
+
+	return nil, nil
+}
+
+func validateSentinelEnvsForAllContainers(sentinel *dbapi.RedisSentinel) error {
+	var err error
+	for _, container := range sentinel.Spec.PodTemplate.Spec.Containers {
+		if errC := amv.ValidateEnvVar(container.Env, forbiddenEnvVars, dbapi.ResourceKindRedisSentinel); errC != nil {
+			if err == nil {
+				err = errC
+			} else {
+				err = errors.Wrap(err, errC.Error())
+			}
+		}
+	}
+	return err
+}
+
+func validateSentinelVolumeMountsForAllContainers(sentinel *dbapi.RedisSentinel) error {
+	var err error
+	for _, container := range sentinel.Spec.PodTemplate.Spec.Containers {
+		if errC := amv.ValidateMountPaths(container.VolumeMounts, reservedMountPaths); errC != nil {
+			if err == nil {
+				err = errC
+			} else {
+				err = errors.Wrap(err, errC.Error())
+			}
+		}
+	}
+	return err
+}
+
+func validateSentinelVersion(version string) error {
+	rdVersion, err := semver.NewVersion(version)
+	if err != nil {
+		return err
+	}
+	if rdVersion.Major() < 6 || (rdVersion.Major() == 6 && rdVersion.Minor() < 2) {
+		return fmt.Errorf("minimum version needed to use kubedb managed redis sentinel is 6.2.5")
+	}
+	return nil
+}
