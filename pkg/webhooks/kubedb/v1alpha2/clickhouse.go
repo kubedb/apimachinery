54cbce854fb31a3931983de10ced7a207ec487a6
SK Ali Arman <arman@appscode.com>
2025-05-09 14:38:28 +0600

    refactor webhook
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- pkg/webhooks/kubedb/v1alpha2/clickhouse.go
+++ pkg/webhooks/kubedb/v1alpha2/clickhouse.go
@@ -124,11 +124,14 @@ func (w *ClickHouseCustomWebhook) ValidateCreateOrUpdate(db *olddbapi.ClickHouse
 		}
 	}
 
-	if db.Spec.TLS != nil && db.Spec.TLS.ClientTLSCaCertRef != nil {
-		if db.Spec.TLS.ClientTLSCaCertRef.Name == "" {
-			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("tls").Child("clientTLSCaCertRef").Child("name"),
-				db.Name,
-				"secret name is missing"))
+	if db.Spec.TLS != nil && db.Spec.TLS.ClientCACertificateRef != nil {
+		for _, secret := range db.Spec.TLS.ClientCACertificateRef {
+			if secret.Name == "" {
+				allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("tls").Child("clientCaCertificateRef").Child("name"),
+					db.Name,
+					"secret name is missing"))
+				return apierrors.NewInvalid(schema.GroupKind{Group: "ClickHouse.kubedb.com", Kind: "ClickHouse"}, db.Name, allErr)
+			}
 		}
 	}
 
