44ac4f35e495cffbd2e2ac5d7deb1880f83aff2e
Hiranmoy Das Chowdhury <49994927+HiranmoyChowdhury@users.noreply.github.com>
2025-05-15 14:50:19 +0600

    Redis Validation code added in webhook (#1457)
    Signed-off-by: Hiranmoy Das Chowdhury <hiranmoy@appscode.com>

--- pkg/webhooks/ops/v1alpha1/redissentinel.go
+++ pkg/webhooks/ops/v1alpha1/redissentinel.go
@@ -20,13 +20,19 @@ import (
 	"context"
 	"fmt"
 
+	"kubedb.dev/apimachinery/apis/catalog/v1alpha1"
 	v1 "kubedb.dev/apimachinery/apis/kubedb/v1"
 	opsapi "kubedb.dev/apimachinery/apis/ops/v1alpha1"
 
+	"github.com/Masterminds/semver/v3"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/mergepatch"
 	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	meta_util "kmodules.xyz/client-go/meta"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -58,7 +64,11 @@ func (w *RedisSentinelOpsRequestCustomWebhook) ValidateCreate(ctx context.Contex
 	}
 
 	redissentinelLog.Info("validate create", "name", req.Name)
-	return nil, w.isDatabaseRefValid(req)
+	err := w.isDatabaseRefValid(req)
+	if err != nil {
+		return nil, err
+	}
+	return nil, w.validateCreateOrUpdate(req)
 }
 
 func (w *RedisSentinelOpsRequestCustomWebhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
@@ -74,7 +84,11 @@ func (w *RedisSentinelOpsRequestCustomWebhook) ValidateUpdate(ctx context.Contex
 	if err := validateRedisSentinelOpsRequest(newReq, oldReq); err != nil {
 		return nil, fmt.Errorf("%v", err)
 	}
-	return nil, w.isDatabaseRefValid(newReq)
+	err := w.isDatabaseRefValid(newReq)
+	if err != nil {
+		return nil, err
+	}
+	return nil, w.validateCreateOrUpdate(newReq)
 }
 
 func (w *RedisSentinelOpsRequestCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
@@ -96,4 +110,55 @@ func validateRedisSentinelOpsRequest(obj, oldObj runtime.Object) error {
 func (w *RedisSentinelOpsRequestCustomWebhook) isDatabaseRefValid(obj *opsapi.RedisSentinelOpsRequest) error {
 	rs := &v1.RedisSentinel{ObjectMeta: metav1.ObjectMeta{Name: obj.Spec.DatabaseRef.Name, Namespace: obj.Namespace}}
 	return w.DefaultClient.Get(context.TODO(), client.ObjectKeyFromObject(rs), rs)
+}
+
+func (w *RedisSentinelOpsRequestCustomWebhook) validateCreateOrUpdate(req *opsapi.RedisSentinelOpsRequest) error {
+	var allErr field.ErrorList
+	switch req.GetRequestType().(opsapi.RedisSentinelOpsRequestType) {
+	case opsapi.RedisSentinelOpsRequestTypeUpdateVersion:
+		if err := w.validateRedisSentinelUpdateVersionOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("updateVersion"),
+				req.Name,
+				err.Error()))
+		}
+	}
+	if len(allErr) == 0 {
+		return nil
+	}
+	return apierrors.NewInvalid(schema.GroupKind{Group: "redissentinelopsrequest.kubedb.com", Kind: "RedisSentinelOpsRequest"}, req.Name, allErr)
+}
+
+func (w *RedisSentinelOpsRequestCustomWebhook) validateRedisSentinelUpdateVersionOpsRequest(req *opsapi.RedisSentinelOpsRequest) error {
+	updatedVersionName := req.Spec.UpdateVersion.TargetVersion
+	redisSentinel := v1.RedisSentinel{}
+	err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: req.GetDBRefName(), Namespace: req.Namespace}, &redisSentinel)
+	if err != nil {
+		return err
+	}
+	currentVersionName := redisSentinel.Spec.Version
+
+	updatedVersion := &v1alpha1.RedisVersion{}
+	if err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: updatedVersionName}, updatedVersion); err != nil {
+		return err
+	}
+
+	currentVersion := &v1alpha1.RedisVersion{}
+	if err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: currentVersionName}, currentVersion); err != nil {
+		return err
+	}
+
+	if updatedVersion.Spec.Distribution != currentVersion.Spec.Distribution {
+		updatedSemver, err := semver.NewVersion(updatedVersion.Spec.Version)
+		if err != nil {
+			return err
+		}
+		currentSemver, err := semver.NewVersion(currentVersion.Spec.Version)
+		if err != nil {
+			return err
+		}
+		if updatedSemver.Major() != currentSemver.Major() {
+			return fmt.Errorf("redisSentinelOpsRequest %s/%s: can't upgrade Official to Valkey with a different major version", req.Namespace, req.Name)
+		}
+	}
+	return nil
 }
