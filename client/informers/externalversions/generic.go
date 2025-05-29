de1412fcb8e2b6582e1266bc166e119c36e8233f
Pritam Das <pritam@appscode.com>
2025-05-29 12:42:41 +0600

    Add hazelcast api (#1413)
    Signed-off-by: pritamdas99 <pritam@appscode.com>

--- client/informers/externalversions/generic.go
+++ client/informers/externalversions/generic.go
@@ -136,6 +136,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().EtcdVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("ferretdbversions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().FerretDBVersions().Informer()}, nil
+	case catalogv1alpha1.SchemeGroupVersion.WithResource("hazelcastversions"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().HazelcastVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("igniteversions"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Catalog().V1alpha1().IgniteVersions().Informer()}, nil
 	case catalogv1alpha1.SchemeGroupVersion.WithResource("kafkaconnectorversions"):
@@ -270,6 +272,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Etcds().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("ferretdbs"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().FerretDBs().Informer()}, nil
+	case v1alpha2.SchemeGroupVersion.WithResource("hazelcasts"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Hazelcasts().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("ignites"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Kubedb().V1alpha2().Ignites().Informer()}, nil
 	case v1alpha2.SchemeGroupVersion.WithResource("kafkas"):
