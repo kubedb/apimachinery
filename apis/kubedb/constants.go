2702a997f004c09fa55f159d2e672bf8d2117af8
Sajjad <sajjadsadi2203@gmail.com>
2025-09-18 10:24:25 +0600

    OracleSharedTlsVolumeMountPath
    Signed-off-by: Sajjad <sajjadsadi2203@gmail.com>

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -2106,14 +2106,15 @@ const (
 
 	OracleStandbyServiceSuffix = "standby"
 
-	OracleDatabaseRoleKey      = "oracle.db/role"
-	OracleDatabaseRoleObserver = "observer"
-	OracleDatabaseRoleInstance = "instance"
-	OracleEnvUserName          = "SYS_USER"
-	OracleEnvPassword          = "ORACLE_PWD"
-	OracleEnvOracleSID         = "ORACLE_SID"
-	OracleEnvDataDir           = "ORADATA"
-	OracleSharedTlsVolumeName  = "certs"
+	OracleDatabaseRoleKey          = "oracle.db/role"
+	OracleDatabaseRoleObserver     = "observer"
+	OracleDatabaseRoleInstance     = "instance"
+	OracleEnvUserName              = "SYS_USER"
+	OracleEnvPassword              = "ORACLE_PWD"
+	OracleEnvOracleSID             = "ORACLE_SID"
+	OracleEnvDataDir               = "ORADATA"
+	OracleSharedTlsVolumeName      = "certs"
+	OracleSharedTlsVolumeMountPath = "/tls/certs"
 )
 
 // =========================== Qdrant Constants ============================
