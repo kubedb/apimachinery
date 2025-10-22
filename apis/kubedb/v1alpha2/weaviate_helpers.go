f649185bb0463d23e2f243b2f7322d0e8ee3319a
tutul <tutul@appscode.com>
2025-10-22 12:56:05 +0600

    weaviate
    Signed-off-by: tutul <tutul@appscode.com>

--- apis/kubedb/v1alpha2/weaviate_helpers.go
+++ apis/kubedb/v1alpha2/weaviate_helpers.go
@@ -20,32 +20,63 @@ import (
 	"fmt"
 
 	"kubedb.dev/apimachinery/apis/kubedb"
+	"kubedb.dev/apimachinery/crds"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/pointer"
+	"kmodules.xyz/client-go/apiextensions"
 	meta_util "kmodules.xyz/client-go/meta"
+	appcat "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	// ofst "kmodules.xyz/offshoot-api/api/v1"
 )
 
-func (w *Weaviate) SetHealthCheckerDefaults() {
-	if w.Spec.HealthChecker.PeriodSeconds == nil {
-		w.Spec.HealthChecker.PeriodSeconds = pointer.Int32(10) // Int32P shows an error.
-	}
-	if w.Spec.HealthChecker.TimeoutSeconds == nil {
-		w.Spec.HealthChecker.TimeoutSeconds = pointer.Int32(10)
-	}
-	if w.Spec.HealthChecker.FailureThreshold == nil {
-		w.Spec.HealthChecker.FailureThreshold = pointer.Int32(3)
-	}
+func (r *Weaviate) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
+	return crds.MustCustomResourceDefinition(SchemeGroupVersion.WithResource(ResourcePluralWeaviate))
+}
+
+type WeaviateApp struct {
+	*Weaviate
+}
+
+func (w WeaviateApp) Name() string {
+	return w.Weaviate.Name
+}
+
+func (w WeaviateApp) Type() appcat.AppType {
+	return appcat.AppType(fmt.Sprintf("%s/%s", kubedb.GroupName, ResourceSingularWeaviate))
+}
+
+func (w *Weaviate) AppBindingMeta() appcat.AppBindingMeta {
+	return &WeaviateApp{w}
+}
+
+func (w *Weaviate) ResourceShortCode() string {
+	return ResourceCodeWeaviate
 }
 
 func (w *Weaviate) ResourceKind() string {
 	return ResourceKindWeaviate
 }
 
+func (w *Weaviate) ResourceSingular() string {
+	return ResourceSingularWeaviate
+}
+
+func (w *Weaviate) ResourcePlural() string {
+	return ResourcePluralWeaviate
+}
+
+func (w *Weaviate) AsOwner() *meta.OwnerReference {
+	return meta.NewControllerRef(w, SchemeGroupVersion.WithKind(ResourceKindRabbitmq))
+}
+
+func (w *Weaviate) ResourceFQN() string {
+	return fmt.Sprintf("%s.%s", w.ResourcePlural(), kubedb.GroupName)
+}
+
 // Owner returns owner reference to resources
-func (w *Weaviate) Owner() *metav1.OwnerReference {
-	return metav1.NewControllerRef(w, SchemeGroupVersion.WithKind(w.ResourceKind()))
+func (w *Weaviate) Owner() *meta.OwnerReference {
+	return meta.NewControllerRef(w, SchemeGroupVersion.WithKind(w.ResourceKind()))
 }
 
 func (w *Weaviate) OffshootName() string {
@@ -56,23 +87,8 @@ func (w *Weaviate) ServiceName() string {
 	return w.OffshootName()
 }
 
-// GetServiceTemplate returns a pointer to the desired serviceTemplate referred by "aliaS". Otherwise, it returns nil.
-//func GetServiceTemplate(templates []NamedServiceTemplateSpec, alias ServiceAlias) ofst.ServiceTemplateSpec {
-//	for i := range templates {
-//		c := templates[i]
-//		if c.Alias == alias {
-//			return c.ServiceTemplateSpec
-//		}
-//	}
-//	return ofst.ServiceTemplateSpec{}
-//}
-
-func (w *Weaviate) ResourcePlural() string {
-	return ResourcePluralWeaviate
-}
-
-func (w *Weaviate) ResourceFQN() string {
-	return fmt.Sprintf("%s.%s", w.ResourcePlural(), kubedb.GroupName)
+func (w *Weaviate) GoverningServiceName() string {
+	return meta_util.NameWithSuffix(w.ServiceName(), "pods")
 }
 
 func (w *Weaviate) offshootLabels(selector, override map[string]string) map[string]string {
@@ -93,8 +109,25 @@ func (w *Weaviate) OffshootLabels() map[string]string {
 	return w.offshootLabels(w.OffshootSelectors(), nil)
 }
 
-func (w *Weaviate) GoverningServiceName() string {
-	return meta_util.NameWithSuffix(w.ServiceName(), "pods")
+func (w *Weaviate) ServiceLabels(alias ServiceAlias, extraLabels ...map[string]string) map[string]string {
+	svcTemplate := GetServiceTemplate(w.Spec.ServiceTemplates, alias)
+	return w.offshootLabels(meta_util.OverwriteKeys(w.OffshootSelectors(), extraLabels...), svcTemplate.Labels)
+}
+
+func (w *Weaviate) PVCName(alias string) string {
+	return meta_util.NameWithSuffix(w.Name, alias)
+}
+
+func (w *Weaviate) SetHealthCheckerDefaults() {
+	if w.Spec.HealthChecker.PeriodSeconds == nil {
+		w.Spec.HealthChecker.PeriodSeconds = pointer.Int32(10) // Int32P shows an error.
+	}
+	if w.Spec.HealthChecker.TimeoutSeconds == nil {
+		w.Spec.HealthChecker.TimeoutSeconds = pointer.Int32(10)
+	}
+	if w.Spec.HealthChecker.FailureThreshold == nil {
+		w.Spec.HealthChecker.FailureThreshold = pointer.Int32(3)
+	}
 }
 
 func (w *Weaviate) PodLabels(extraLabels ...map[string]string) map[string]string {
@@ -103,4 +136,15 @@ func (w *Weaviate) PodLabels(extraLabels ...map[string]string) map[string]string
 
 func (w *Weaviate) PetSetName() string {
 	return w.OffshootName()
+}
+
+func (w *Weaviate) GetAuthSecretName() string {
+	if w.Spec.AuthSecret != nil && w.Spec.AuthSecret.Name != "" {
+		return w.Spec.AuthSecret.Name
+	}
+	return w.DefaultUserCredSecretName()
+}
+
+func (m *Weaviate) DefaultUserCredSecretName() string {
+	return meta_util.NameWithSuffix(m.OffshootName(), "auth")
 }
