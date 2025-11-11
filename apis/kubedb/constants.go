1d07d3d3b1b85c018c9f16d42bd152325d275e6b
urmi <urmi@appscode.com>
2025-11-11 15:59:29 +0600

    milvus constants updated

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -887,10 +887,46 @@ const (
 
 const (
 	MilvusUsernameKey = "username"
+	MilvusUsername    = "root"
 	MilvusPasswordKey = "password"
+	MilvusPassword    = "Milvus"
 
 	MilvusPrimaryServicePortName = "primary"
 	MilvusServicePort            = 19530
+
+	MilvusGrpcPortName = "grpc"
+	MilvusGrpcPort     = int32(19530)
+
+	MilvusMetricsPortName = "metrics"
+	MilvusMetricsPort     = int32(9091)
+
+	MilvusPVCName = "milvus-pvc"
+	MilvusPVCDir  = "/var/lib/milvus"
+
+	MilvusConfigVolName = "milvus-config"
+	MilvusConfigDir     = "/milvus/configs/milvus.yaml"
+
+	MilvusWritableVolName = "milvus-writable"
+	MilvusWritableDir     = "/milvus"
+
+	MilvusConfigFileName = "milvus.yaml"
+
+	MilvusStorageName = "milvus-storage"
+
+	MilvusDefaultImage = "milvusdb/milvus:v2.6.1"
+
+	MilvusContainerName = "milvus"
+
+	MilvusInitContainerName = "fix-permissions"
+
+	EtcdEndpointsName = "ETCD_ENDPOINTS"
+
+	MinioAddressName   = "MINIO_ADDRESS"
+	MinioAddressKey    = "address"
+	MinioAccessKeyName = "MINIO_ACCESS_KEY"
+	MinioAccessKey     = "accessKeyID"
+	MinioSecretKeyName = "MINIO_SECRET_KEY"
+	MinioSecretKey     = "secretAccessKey"
 )
 
 const (
