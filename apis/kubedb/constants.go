3f8f352e4691f620c9addfd62af077b31b960089
tutul <tutul@appscode.com>
2025-11-18 18:22:31 +0600

    config-secret
    Signed-off-by: tutul <tutul@appscode.com>

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -1553,6 +1553,8 @@ const (
 	WeaviateLoopBackUserKey            = "loopback_users"
 	WeaviateLoopBackUserVal            = "none"
 	WeaviateAPIKey                     = "AUTHENTICATION_APIKEY_ALLOWED_KEYS"
+
+	WeaviateConfigFileName = "config.yaml"
 )
 
 // =========================== FerretDB Constants ============================
