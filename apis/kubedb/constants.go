7ceb42d260f6f58f0ee1b771880c8c26303d9bb0
tutul <tutul@appscode.com>
2025-11-20 14:06:17 +0600

    add constant
    Signed-off-by: tutul <tutul@appscode.com>

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -1524,6 +1524,8 @@ const (
 	WeaviateDataDir       = "/weaviate/storage"
 	WeaviateContainerName = "weaviate"
 	WeaviateAPIKey        = "AUTHENTICATION_APIKEY_ALLOWED_KEYS"
+	WeaviateAPIKeyEnabled = "AUTHENTICATION_APIKEY_ENABLED"
+	WeaviateAPIKeyUsers   = "AUTHENTICATION_APIKEY_USERS"
 )
 
 // =========================== FerretDB Constants ============================
