2b0deff41c63d14dedfe4b6c7d88a5bfa4431b0c
sabnaj <sabnaj@appscode.com>
2025-12-12 15:34:57 +0600

    db2-hadr-api

--- apis/kubedb/v1alpha2/db2_hadr_types.go
+++ apis/kubedb/v1alpha2/db2_hadr_types.go
@@ -1,1 +1,101 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kmapi "kmodules.xyz/client-go/api/v1"
+)
+
+//
+//const (
+//	ResourceCodeDB2     = "db2"
+//	ResourceKindDB2     = "DB2"
+//	ResourceSingularDB2 = "db2"
+//	ResourcePluralDB2   = "db2s"
+//)
+
+// DB2 is the Schema for the db2s API.
+
+// +genclient
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=db2s,singular=db2,shortName=db2,categories={datastore,kubedb,appscode,all}
+// +kubebuilder:printcolumn:name="Type",type="string",JSONPath=".apiVersion"
+// +kubebuilder:printcolumn:name="Version",type="string",JSONPath=".spec.version"
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+type DB2Hadr struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DB2HadrSpec   `json:"spec,omitempty"`
+	Status DB2HadrStatus `json:"status,omitempty"`
+}
+
+// DB2Spec defines the desired state of DB2.
+type DB2HadrSpec struct {
+	Primary PrimarySpec `json:"primary,omitempty"`
+	Standby StandbySpec `json:"standby,omitempty"`
+	//+optional
+	configSecrcet SecretReference `json:"configSecret,omitempty"`
+	// ServiceTemplates is an optional configuration for services used to expose database
+	// +optional
+	ServiceTemplates []NamedServiceTemplateSpec `json:"serviceTemplates,omitempty"`
+}
+
+// DB2HadrStatus defines the observed state of DB2.
+type DB2HadrStatus struct {
+	// Specifies the current phase of the database
+	// +optional
+	Phase DatabasePhase `json:"phase,omitempty"`
+	// observedGeneration is the most recent generation observed for this resource. It corresponds to the
+	// resource's generation, which is updated on mutation by the API Server.
+	// +optional
+	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
+	// Conditions applied to the database, such as approval or denial.
+	// +optional
+	Conditions []kmapi.Condition `json:"conditions,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+
+// DB2HadrList contains a list of DB2Hadr.
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type DB2HadrList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DB2Hadr `json:"items"`
+}
+
+// +kubebuilder:validation:Enum=coordinators;overlords;brokers;routers;middleManagers;historicals
+type PrimarySpec struct {
+	// Numerical ID
+	// +optional
+	ID int `json:"id"`
+}
+
+type StandbySpec struct {
+	// Numerical ID
+	// +optional
+	ID int `json:"id"`
+}
