5625733d901b144e808911200d57e74d8e1b1041
Masood <masood@appscode.com>
2025-12-23 09:42:49 +0600

    http client tls
    Signed-off-by: Masood <masood@appscode.com>

--- pkg/utils/http/qdrant/client.go
+++ pkg/utils/http/qdrant/client.go
@@ -18,12 +18,16 @@ package qdrant
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"kubedb.dev/apimachinery/apis/kubedb"
 	api "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
 
@@ -57,8 +61,72 @@ type RaftInfo struct {
 	IsVoter           bool   `json:"is_voter,omitempty"`
 }
 
-func GetClusterResponse(ctx context.Context, address string, apiKey string) (*ClusterResponse, error) {
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://"+address+"/cluster", nil)
+
+func GetClusterResponse(
+	ctx context.Context,
+	db *api.Qdrant,
+	kc client.Client,
+	address string,
+) (*ClusterResponse, error) {
+
+	scheme := "http"
+	var transport *http.Transport
+
+	if db.Spec.TLS != nil {
+		scheme = "https"
+
+		caSecret := &corev1.Secret{}
+		err := kc.Get(ctx, types.NamespacedName{
+			Name:      db.GetCertSecretName(api.QdrantServerCert),
+			Namespace: db.Namespace,
+		}, caSecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get server cert secret: %w", err)
+		}
+
+		caPool := x509.NewCertPool()
+		if !caPool.AppendCertsFromPEM(caSecret.Data["ca.crt"]) {
+			return nil, fmt.Errorf("failed to append server CA cert")
+		}
+
+		tlsConfig := &tls.Config{
+			RootCAs: caPool,
+		}
+
+		if db.Spec.TLS.ClientHTTPTLS != nil && *db.Spec.TLS.ClientHTTPTLS {
+			clientSecret := &corev1.Secret{}
+			err := kc.Get(ctx, types.NamespacedName{
+				Name:      db.GetCertSecretName(api.QdrantClientCert),
+				Namespace: db.Namespace,
+			}, clientSecret)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get client cert secret: %w", err)
+			}
+
+			cert, err := tls.X509KeyPair(
+				clientSecret.Data["client.crt"],
+				clientSecret.Data["client.key"],
+			)
+			if err != nil {
+				return nil, fmt.Errorf("invalid client cert/key: %w", err)
+			}
+
+			tlsConfig.Certificates = []tls.Certificate{cert}
+		}
+
+		transport = &http.Transport{
+			TLSClientConfig: tlsConfig,
+		}
+	}
+
+	apiKey := db.GetAPIKey(ctx, kc)
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodGet,
+		fmt.Sprintf("%s://%s/cluster", scheme, address),
+		nil,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -66,22 +134,25 @@ func GetClusterResponse(ctx context.Context, address string, apiKey string) (*Cl
 	if apiKey != "" {
 		req.Header.Set("api-key", apiKey)
 	}
-
 	req.Header.Set("Accept", "application/json")
 
 	client := &http.Client{
-		Timeout: 5 * time.Second,
+		Timeout:   5 * time.Second,
+		Transport: transport,
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("request failed: %w", err)
 	}
+	defer res.Body.Close()
 
-	defer res.Body.Close() // nolint:errcheck
-
-	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("bad status %d from %s (try checking API key or headers)", res.StatusCode, address)
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(
+			"bad status %d from %s",
+			res.StatusCode,
+			address,
+		)
 	}
 
 	var cr ClusterResponse
@@ -94,9 +165,8 @@ func GetClusterResponse(ctx context.Context, address string, apiKey string) (*Cl
 
 func GetClusterStatus(ctx context.Context, db *api.Qdrant, kc client.Client) (string, int, []string, error) {
 	address := db.ServiceDNS() + ":" + strconv.Itoa(kubedb.QdrantHTTPPort)
-	apiKey := db.GetAPIKey(ctx, kc)
 
-	cr, err := GetClusterResponse(ctx, address, apiKey)
+	cr, err := GetClusterResponse(ctx, db, kc, address)
 	if err != nil {
 		return "", 0, nil, err
 	}
@@ -106,7 +176,7 @@ func GetClusterStatus(ctx context.Context, db *api.Qdrant, kc client.Client) (st
 
 	for i := 0; i < replicaCount; i++ {
 		podAddress := db.GetPodAddress(i)
-		cr, err = GetClusterResponse(ctx, podAddress, apiKey)
+		cr, err = GetClusterResponse(ctx, db, kc, podAddress)
 		if err != nil {
 			return "", 0, nil, err
 		}
