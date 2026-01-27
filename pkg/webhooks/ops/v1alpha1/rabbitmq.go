e641688470f89b15483a129026463dab8ed57b77
urmi <urmi@appscode.com>
2026-01-27 19:22:42 +0600

    add rabbitmq
    Signed-off-by: urmi <urmi@appscode.com>

--- pkg/webhooks/ops/v1alpha1/rabbitmq.go
+++ pkg/webhooks/ops/v1alpha1/rabbitmq.go
@@ -85,7 +85,19 @@ func (w *RabbitMQOpsRequestCustomWebhook) ValidateUpdate(ctx context.Context, ol
 	if err := validateRabbitMQOpsRequest(ops, oldOps); err != nil {
 		return nil, err
 	}
-	return nil, w.validateCreateOrUpdate(ops)
+	if err := w.validateCreateOrUpdate(ops); err != nil {
+		return nil, err
+	}
+
+	if isOpsReqCompleted(ops.Status.Phase) && !isOpsReqCompleted(oldOps.Status.Phase) { // just completed
+		var db olddbapi.RabbitMQ
+		err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: ops.Spec.DatabaseRef.Name, Namespace: ops.Namespace}, &db)
+		if err != nil {
+			return nil, err
+		}
+		return nil, resumeDatabase(w.DefaultClient, &db)
+	}
+	return nil, nil
 }
 
 func (w *RabbitMQOpsRequestCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
