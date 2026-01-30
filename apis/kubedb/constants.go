1047f5454d71216cabb745c2456f62489097fdd4
ZobayerAbedin <zobayerabedin@gmail.com>
2026-01-30 14:58:23 +0600

    Add custom configuration for HanaDB
    Signed-off-by: ZobayerAbedin <zobayerabedin@gmail.com>

--- apis/kubedb/constants.go
+++ apis/kubedb/constants.go
@@ -2255,10 +2255,14 @@ const (
 	// Mount paths
 	HanaDBDataDir         = "/hana/mounts"
 	HanaDBSecretMountPath = "/etc/hana-secrets"
+	HanaDBConfigFileName  = "global.ini"
+	HanaDBConfigDir       = "/hana/mounts/system/config"
+	HanaDBConfigMountPath = "/etc/hanadb-config"
 
 	// Volume names
 	HanaDBDataVolume           = "data"
 	HanaDBVolumePasswordSecret = "password-secret"
+	HanaDBConfigVolumeName     = "hanadb-config"
 
 	// User and Group IDs
 	HanaDBUserID  = 12000 // hxeadm UID
@@ -2287,10 +2291,8 @@ const (
 
 	HanaDBPrimaryServicePortName = "primary"
 
-	HanaDBCoordinatorPort           = 2380
-	HanaDBCoordinatorClientPort     = 2379
-	HanaDBCoordinatorPortName       = "coordinator"
-	HanaDBCoordinatorClientPortName = "coordinator-client"
+	HanaDBCoordinatorPort       = 2380
+	HanaDBCoordinatorClientPort = 2379
 
 	// TenantDatabaseName is the name of the KubeDB managed tenant database
 	KubeDBTenantDatabaseName = "KUBEDB_HEALTH_CHECK"
--- pkg/webhooks/kubedb/v1alpha2/hanadb.go
+++ pkg/webhooks/kubedb/v1alpha2/hanadb.go
@@ -26,6 +26,7 @@ import (
 	amv "kubedb.dev/apimachinery/pkg/validator"
 
 	"github.com/pkg/errors"
+	core "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -213,6 +214,19 @@ func (w *HanaDBCustomWebhook) ValidateCreateOrUpdate(db *api.HanaDB) field.Error
 		allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("storage"), db.Name, err.Error()))
 	}
 
+	if db.Spec.Configuration != nil && db.Spec.Configuration.SecretName != "" {
+		configSecret := &core.Secret{}
+		err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+			Name:      db.Spec.Configuration.SecretName,
+			Namespace: db.Namespace,
+		}, configSecret)
+		if err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("configuration").Child("secretName"),
+				db.Spec.Configuration.SecretName,
+				"failed to get configuration secret"))
+		}
+	}
+
 	if len(allErr) == 0 {
 		return nil
 	}
@@ -225,11 +239,14 @@ var hanadbReservedVolumes = []string{
 	kubedb.HanaDBDataVolume,
 	kubedb.HanaDBVolumeScripts,
 	kubedb.HanaDBVolumePasswordSecret,
+	kubedb.HanaDBConfigVolumeName,
 }
 
 var hanadbReservedVolumesMountPaths = []string{
 	kubedb.HanaDBDataDir,
 	kubedb.HanaDBVolumeMountScripts,
+	kubedb.HanaDBConfigDir,
+	kubedb.HanaDBConfigMountPath,
 }
 
 func (w *HanaDBCustomWebhook) hanadbValidateVersion(db *api.HanaDB) error {
