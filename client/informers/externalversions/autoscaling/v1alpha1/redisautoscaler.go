74264e692bccc00739a52d2ac09da9ad59fd7236
Anisur Rahman <anisur@appscode.com>
2026-02-05 18:33:56 +0600

    Fix CI
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- client/informers/externalversions/autoscaling/v1alpha1/redisautoscaler.go
+++ client/informers/externalversions/autoscaling/v1alpha1/redisautoscaler.go
@@ -22,14 +22,15 @@ import (
 	"context"
 	time "time"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	runtime "k8s.io/apimachinery/pkg/runtime"
-	watch "k8s.io/apimachinery/pkg/watch"
-	cache "k8s.io/client-go/tools/cache"
 	autoscalingv1alpha1 "kubedb.dev/apimachinery/apis/autoscaling/v1alpha1"
 	versioned "kubedb.dev/apimachinery/client/clientset/versioned"
 	internalinterfaces "kubedb.dev/apimachinery/client/informers/externalversions/internalinterfaces"
 	v1alpha1 "kubedb.dev/apimachinery/client/listers/autoscaling/v1alpha1"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+	watch "k8s.io/apimachinery/pkg/watch"
+	cache "k8s.io/client-go/tools/cache"
 )
 
 // RedisAutoscalerInformer provides access to a shared informer and lister for
