d20a99315aed4ad7bf17765f465983637a1bffe1
Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>
2026-04-09 13:06:05 +0600

    Qdrant Ops Request  (#1631)
    * types added
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * tls spec change
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * webhook
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * go-client change
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * changes
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * update_version webhook
    
    Signed-off-by: masood <aalmasood22@gmail.com>
    
    * vertical scaling ops
    
    Signed-off-by: masood <aalmasood22@gmail.com>
    
    * lint
    
    Signed-off-by: masood <aalmasood22@gmail.com>
    
    * bug fix
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * all webhooks
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * fixes
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * fix
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * remove peer
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * constant add
    
    Signed-off-by: Masood <masood@appscode.com>
    
    * review changes
    
    Signed-off-by: Masood <masood@appscode.com>
    
    ---------
    
    Signed-off-by: Masood <masood@appscode.com>
    Signed-off-by: masood <aalmasood22@gmail.com>
    Signed-off-by: souravbiswassanto <saurov@appscode.com>
    Co-authored-by: souravbiswassanto <saurov@appscode.com>

---
+++ pkg/webhooks/ops/v1alpha1/qdrant.go
@@ -0,0 +1,357 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
+	catalog "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
+	"kubedb.dev/apimachinery/apis/kubedb"
+	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1alpha2"
+	opsapi "kubedb.dev/apimachinery/apis/ops/v1alpha1"
+
+	"gomodules.xyz/x/arrays"
+	core "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/mergepatch"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	metautil "kmodules.xyz/client-go/meta"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+func SetupQdrantOpsRequestWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&opsapi.QdrantOpsRequest{}).
+		WithValidator(&QdrantOpsRequestCustomWebhook{mgr.GetClient()}).
+		Complete()
+}
+
+type QdrantOpsRequestCustomWebhook struct {
+	DefaultClient client.Client
+}
+
+var qdrantOpsReqLog = logf.Log.WithName("qdrant-opsrequest")
+
+var _ webhook.CustomValidator = &QdrantOpsRequestCustomWebhook{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (w *QdrantOpsRequestCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	ops, ok := obj.(*opsapi.QdrantOpsRequest)
+	if !ok {
+		return nil, fmt.Errorf("expected an QdrantOpsRequest object but got %T", obj)
+	}
+	qdrantOpsReqLog.Info("validate create", "name", ops.Name)
+	return nil, w.validateCreateOrUpdate(ops)
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (w *QdrantOpsRequestCustomWebhook) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+	ops, ok := newObj.(*opsapi.QdrantOpsRequest)
+	if !ok {
+		return nil, fmt.Errorf("expected an QdrantOpsRequest object but got %T", newObj)
+	}
+	qdrantOpsReqLog.Info("validate update", "name", ops.Name)
+
+	oldOps, ok := oldObj.(*opsapi.QdrantOpsRequest)
+	if !ok {
+		return nil, fmt.Errorf("expected an QdrantOpsRequest object but got %T", oldObj)
+	}
+
+	if err := validateQdrantOpsRequest(ops, oldOps); err != nil {
+		return nil, err
+	}
+
+	if err := w.validateCreateOrUpdate(ops); err != nil {
+		return nil, err
+	}
+
+	if isOpsReqCompleted(ops.Status.Phase) && !isOpsReqCompleted(oldOps.Status.Phase) { // just completed
+		var db dbapi.Qdrant
+		err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{Name: ops.Spec.DatabaseRef.Name, Namespace: ops.Namespace}, &db)
+		if err != nil {
+			return nil, err
+		}
+		return nil, resumeDatabase(w.DefaultClient, &db)
+	}
+	return nil, nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	return nil, nil
+}
+
+func validateQdrantOpsRequest(req *opsapi.QdrantOpsRequest, oldReq *opsapi.QdrantOpsRequest) error {
+	preconditions := metautil.PreConditionSet{Set: sets.New[string]("spec")}
+	_, err := metautil.CreateStrategicPatch(oldReq, req, preconditions.PreconditionFunc()...)
+	if err != nil {
+		if mergepatch.IsPreconditionFailed(err) {
+			return fmt.Errorf("%v.%v", err, preconditions.Error())
+		}
+		return err
+	}
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateCreateOrUpdate(req *opsapi.QdrantOpsRequest) error {
+	if validType, _ := arrays.Contains(opsapi.QdrantOpsRequestTypeNames(), string(req.Spec.Type)); !validType {
+		return field.Invalid(field.NewPath("spec").Child("type"), req.Name,
+			fmt.Sprintf("defined OpsRequestType %s is not supported, supported types for Qdrant are %s", req.Spec.Type, strings.Join(opsapi.QdrantOpsRequestTypeNames(), ", ")))
+	}
+
+	db, err := w.hasDatabaseRef(req)
+	if err != nil {
+		return field.Invalid(field.NewPath("spec").Child("databaseRef"), req.Name, err.Error())
+	}
+
+	var allErr field.ErrorList
+	switch opsapi.QdrantOpsRequestType(req.GetRequestType()) {
+	case opsapi.QdrantOpsRequestTypeHorizontalScaling:
+		if err := w.validateQdrantHorizontalScalingOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("horizontalScaling"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeReconfigure:
+		if err := w.validateQdrantReconfigurationOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("configuration"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeReconfigureTLS:
+		if err := w.validateQdrantReconfigureTLSOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("tls"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeRestart:
+
+	case opsapi.QdrantOpsRequestTypeRotateAuth:
+		if err := w.validateQdrantRotateAuthOpsRequest(db, req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("authentication"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeUpdateVersion:
+		if err := w.validateQdrantUpdateVersionOpsRequest(db, req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("updateVersion"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeVerticalScaling:
+		if err := w.validateQdrantVerticalScalingOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("verticalScaling"),
+				req.Name,
+				err.Error()))
+		}
+
+	case opsapi.QdrantOpsRequestTypeVolumeExpansion:
+		if err := w.validateQdrantVolumeExpansionOpsRequest(req); err != nil {
+			allErr = append(allErr, field.Invalid(field.NewPath("spec").Child("volumeExpansion"),
+				req.Name,
+				err.Error()))
+		}
+	}
+
+	if len(allErr) == 0 {
+		return nil
+	}
+	return apierrors.NewInvalid(schema.GroupKind{Group: "qdrantopsrequests.kubedb.com", Kind: "QdrantOpsRequest"}, req.Name, allErr)
+}
+
+func (w *QdrantOpsRequestCustomWebhook) hasDatabaseRef(req *opsapi.QdrantOpsRequest) (*dbapi.Qdrant, error) {
+	qdrant := &dbapi.Qdrant{}
+	if err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+		Name:      req.GetDBRefName(),
+		Namespace: req.GetNamespace(),
+	}, qdrant); err != nil {
+		return nil, fmt.Errorf("spec.databaseRef %s/%s, is invalid or not found", req.GetNamespace(), req.GetDBRefName())
+	}
+
+	return qdrant, nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantHorizontalScalingOpsRequest(req *opsapi.QdrantOpsRequest) error {
+	horizontalScalingSpec := req.Spec.HorizontalScaling
+	if horizontalScalingSpec == nil {
+		return errors.New("spec.horizontalScaling nil not supported in HorizontalScaling type")
+	}
+
+	if horizontalScalingSpec.Node == nil {
+		return errors.New("spec.horizontalScaling.node can not be empty")
+	}
+
+	if *horizontalScalingSpec.Node <= 0 {
+		return errors.New("spec.horizontalScaling.node must be positive")
+	}
+
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantReconfigurationOpsRequest(req *opsapi.QdrantOpsRequest) error {
+	reconfigureSpec := req.Spec.Configuration
+	if reconfigureSpec == nil {
+		return errors.New("spec configuration nil not supported in Reconfigure type")
+	}
+
+	if !reconfigureSpec.RemoveCustomConfig && reconfigureSpec.ConfigSecret == nil && len(reconfigureSpec.ApplyConfig) == 0 {
+		return errors.New("at least one of `RemoveCustomConfig`, `ConfigSecret`, or `ApplyConfig` must be specified")
+	}
+
+	if reconfigureSpec.ConfigSecret != nil && reconfigureSpec.ConfigSecret.Name != "" {
+		var secret core.Secret
+		err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+			Name:      reconfigureSpec.ConfigSecret.Name,
+			Namespace: req.Namespace,
+		}, &secret)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return fmt.Errorf("referenced config secret %s/%s not found", req.Namespace, reconfigureSpec.ConfigSecret.Name)
+			}
+			return err
+		}
+
+		if _, ok := secret.Data[kubedb.QdrantConfigFileName]; !ok {
+			return fmt.Errorf("config secret %s/%s does not have file named '%v'", req.Namespace, reconfigureSpec.ConfigSecret.Name, kubedb.QdrantConfigFileName)
+		}
+	}
+
+	// Validate ApplyConfig has the required config file if provided
+	if req.Spec.Configuration.ApplyConfig != nil {
+		_, ok := req.Spec.Configuration.ApplyConfig[kubedb.QdrantConfigFileName]
+		if !ok {
+			return fmt.Errorf("`spec.configuration.applyConfig` does not have file named '%v'", kubedb.QdrantConfigFileName)
+		}
+	}
+
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantReconfigureTLSOpsRequest(req *opsapi.QdrantOpsRequest) error {
+	TLSSpec := req.Spec.TLS
+	if TLSSpec == nil {
+		return errors.New("spec.TLS nil not supported in ReconfigureTLS type")
+	}
+
+	configCount := 0
+	if TLSSpec.Client != nil && *TLSSpec.Client {
+		configCount++
+	}
+	if TLSSpec.P2P != nil && *TLSSpec.P2P {
+		configCount++
+	}
+	if TLSSpec.Remove {
+		configCount++
+	}
+	if TLSSpec.RotateCertificates {
+		configCount++
+	}
+	if TLSSpec.IssuerRef != nil || TLSSpec.Certificates != nil {
+		configCount++
+	}
+
+	if configCount == 0 {
+		return errors.New("no reconfiguration is provided in TLS spec")
+	}
+
+	if configCount > 1 {
+		return errors.New("more than 1 field have assigned to spec.reconfigureTLS but at a time one is allowed to run one operation")
+	}
+
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantRotateAuthOpsRequest(db *dbapi.Qdrant, req *opsapi.QdrantOpsRequest) error {
+	if db.Spec.DisableSecurity {
+		return fmt.Errorf("disableSecurity is on, RotateAuth is not applicable")
+	}
+
+	authSpec := req.Spec.Authentication
+	if authSpec != nil && authSpec.SecretRef != nil {
+		if authSpec.SecretRef.Name == "" {
+			return errors.New("spec.authentication.secretRef.name can not be empty")
+		}
+		err := w.DefaultClient.Get(context.TODO(), types.NamespacedName{
+			Name:      authSpec.SecretRef.Name,
+			Namespace: req.Namespace,
+		}, &core.Secret{})
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				return fmt.Errorf("referenced secret %s not found", authSpec.SecretRef.Name)
+			}
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantUpdateVersionOpsRequest(db *dbapi.Qdrant, req *opsapi.QdrantOpsRequest) error {
+	updateVersionSpec := req.Spec.UpdateVersion
+	if updateVersionSpec == nil {
+		return errors.New("spec.updateVersion nil not supported in UpdateVersion type")
+	}
+
+	yes, err := IsUpgradable(w.DefaultClient, catalog.ResourceKindQdrantVersion, db.Spec.Version, updateVersionSpec.TargetVersion)
+	if err != nil {
+		return err
+	}
+	if !yes {
+		return fmt.Errorf("upgrade from version %v to %v is not supported", db.Spec.Version, req.Spec.UpdateVersion.TargetVersion)
+	}
+
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantVerticalScalingOpsRequest(req *opsapi.QdrantOpsRequest) error {
+	verticalScalingSpec := req.Spec.VerticalScaling
+	if verticalScalingSpec == nil {
+		return errors.New("spec.verticalScaling nil not supported in VerticalScaling type")
+	}
+
+	if verticalScalingSpec.Node == nil {
+		return errors.New("spec.verticalScaling.Node can't be empty")
+	}
+
+	return nil
+}
+
+func (w *QdrantOpsRequestCustomWebhook) validateQdrantVolumeExpansionOpsRequest(req *opsapi.QdrantOpsRequest) error {
+	volumeExpansionSpec := req.Spec.VolumeExpansion
+	if volumeExpansionSpec == nil {
+		return errors.New("spec.volumeExpansion nil not supported in VolumeExpansion type")
+	}
+
+	if volumeExpansionSpec.Node == nil {
+		return errors.New("spec.volumeExpansion.Node can't be empty")
+	}
+
+	return nil
+}
