25e64308f785e34b4734e85b7c8ae5ea60586ed6
sabnaj <sabnaj@appscode.com>
2026-04-09 12:53:24 +0600

    build pass
    Signed-off-by: sabnaj <sabnaj@appscode.com>

--- client/clientset/versioned/typed/migrator/v1alpha1/fake/fake_migrator_client.go
+++ client/clientset/versioned/typed/migrator/v1alpha1/fake/fake_migrator_client.go
@@ -19,9 +19,10 @@ limitations under the License.
 package fake
 
 import (
+	v1alpha1 "kubedb.dev/apimachinery/client/clientset/versioned/typed/migrator/v1alpha1"
+
 	rest "k8s.io/client-go/rest"
 	testing "k8s.io/client-go/testing"
-	v1alpha1 "kubedb.dev/apimachinery/client/clientset/versioned/typed/migrator/v1alpha1"
 )
 
 type FakeMigratorV1alpha1 struct {
