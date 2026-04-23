564b9e160f7b5212187624d4423a626fe31a4d3f
Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>
2026-04-23 09:53:09 +0600

    autoscaler webhook
    Signed-off-by: Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>

---
+++ pkg/webhooks/autoscaling/v1alpha1/qdrant.go
@@ -0,0 +1,122 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	autoscalingapi "kubedb.dev/apimachinery/apis/autoscaling/v1alpha1"
+	opsapi "kubedb.dev/apimachinery/apis/ops/v1alpha1"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// SetupQdrantAutoscalerWebhookWithManager registers the webhook for QdrantAutoscaler in the manager.
+func SetupQdrantAutoscalerWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).For(&autoscalingapi.QdrantAutoscaler{}).
+		WithValidator(&QdrantAutoscalerCustomWebhook{mgr.GetClient()}).
+		WithDefaulter(&QdrantAutoscalerCustomWebhook{mgr.GetClient()}).
+		Complete()
+}
+
+type QdrantAutoscalerCustomWebhook struct {
+	DefaultClient client.Client
+}
+
+// log is for logging in this package.
+var QdrantLog = logf.Log.WithName("qdrant-autoscaler")
+
+// +kubebuilder:webhook:path=/mutate-autoscaling-kubedb-com-v1alpha1-qdrantautoscaler,mutating=true,failurePolicy=fail,sideEffects=None,groups=autoscaling.kubedb.com,resources=qdrantautoscaler,verbs=create;update,versions=v1alpha1,name=mqdrantautoscaler.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.CustomDefaulter = &QdrantAutoscalerCustomWebhook{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type
+func (w *QdrantAutoscalerCustomWebhook) Default(ctx context.Context, obj runtime.Object) error {
+	scaler, ok := obj.(*autoscalingapi.QdrantAutoscaler)
+	if !ok {
+		return fmt.Errorf("expected an QdrantAutoscaler object but got %T", obj)
+	}
+
+	QdrantLog.Info("defaulting", "name", scaler.Name)
+	w.setDefaults(scaler)
+	return nil
+}
+
+func (w *QdrantAutoscalerCustomWebhook) setDefaults(scaler *autoscalingapi.QdrantAutoscaler) {
+	w.setOpsReqOptsDefaults(scaler)
+
+	if scaler.Spec.Storage != nil {
+		setDefaultStorageValues(scaler.Spec.Storage.Node)
+	}
+	if scaler.Spec.Compute != nil {
+		setDefaultComputeValues(scaler.Spec.Compute.Node)
+	}
+}
+
+func (w *QdrantAutoscalerCustomWebhook) setOpsReqOptsDefaults(scaler *autoscalingapi.QdrantAutoscaler) {
+	if scaler.Spec.OpsRequestOptions == nil {
+		scaler.Spec.OpsRequestOptions = &autoscalingapi.QdrantOpsRequestOptions{}
+	}
+	// Timeout is defaulted to 600s w ops-manager retries.go (to retry 120 times with 5sec pause between each)
+	// OplogMaxLagSeconds & ObjectsCountDiffPercentage are defaults to 0
+	if scaler.Spec.OpsRequestOptions.Apply == "" {
+		scaler.Spec.OpsRequestOptions.Apply = opsapi.ApplyOptionIfReady
+	}
+}
+
+// +kubebuilder:webhook:path=/validate-schema-kubedb-com-v1alpha1-qdrantautoscaler,mutating=false,failurePolicy=fail,sideEffects=None,groups=schema.kubedb.com,resources=qdrantautoscalers,verbs=create;update;delete,versions=v1alpha1,name=vqdrantautoscaler.kb.io,admissionReviewVersions={v1,v1beta1}
+
+var _ webhook.CustomValidator = &QdrantAutoscalerCustomWebhook{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (w *QdrantAutoscalerCustomWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	scaler, ok := obj.(*autoscalingapi.QdrantAutoscaler)
+	if !ok {
+		return nil, fmt.Errorf("expected an QdrantAutoscaler object but got %T", obj)
+	}
+
+	QdrantLog.Info("validate create", "name", scaler.Name)
+	return nil, w.validate(scaler)
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (w *QdrantAutoscalerCustomWebhook) ValidateUpdate(ctx context.Context, old, newObj runtime.Object) (admission.Warnings, error) {
+	scaler, ok := newObj.(*autoscalingapi.QdrantAutoscaler)
+	if !ok {
+		return nil, fmt.Errorf("expected an QdrantAutoscaler object but got %T", newObj)
+	}
+
+	return nil, w.validate(scaler)
+}
+
+func (w QdrantAutoscalerCustomWebhook) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	return nil, nil
+}
+
+func (w *QdrantAutoscalerCustomWebhook) validate(scaler *autoscalingapi.QdrantAutoscaler) error {
+	if scaler.Spec.DatabaseRef == nil {
+		return errors.New("databaseRef can't be empty")
+	}
+	return nil
+}
