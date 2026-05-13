919b055a03fb5a8e9a22264be4ac2b39a162e2ea
Rabbani Islam Refat <refat@appscode.com>
2026-05-13 18:07:57 +0600

    use mysql api definition for mariadb
    Signed-off-by: Rabbani Islam Refat <refat@appscode.com>

--- apis/migrator/v1alpha1/mariadb.go
+++ apis/migrator/v1alpha1/mariadb.go
@@ -18,53 +18,14 @@ package v1alpha1
 
 type MariaDBSource struct {
 	// ConnectionInfo refers to the source MariaDB database connection information.
-	ConnectionInfo *MariaDBConnectionInfo `yaml:"connectionInfo" json:"connectionInfo"`
-	Schema         *MariaDBSchema         `yaml:"schema" json:"schema,omitempty"`
-	Snapshot       *MariaDBSnapshot       `yaml:"snapshot" json:"snapshot,omitempty"`
-	Streaming      *MariaDBStreaming      `yaml:"streaming" json:"streaming,omitempty"`
+	ConnectionInfo *MySQLConnectionInfo `yaml:"connectionInfo" json:"connectionInfo"`
+	Schema         *MySQLSchema         `yaml:"schema" json:"schema,omitempty"`
+	Snapshot       *MySQLSnapshot       `yaml:"snapshot" json:"snapshot,omitempty"`
+	Streaming      *MySQLStreaming      `yaml:"streaming" json:"streaming,omitempty"`
 }
 
 type MariaDBTarget struct {
 	// ConnectionInfo refers to the target MariaDB database connection information.
-	ConnectionInfo *MariaDBConnectionInfo `yaml:"connectionInfo" json:"connectionInfo"`
+	ConnectionInfo *MySQLConnectionInfo `yaml:"connectionInfo" json:"connectionInfo"`
 }
 
-type MariaDBSchema struct {
-	// Enabled controls whether the Schema Phase should be executed.
-	Enabled bool `yaml:"enabled" json:"enabled"`
-	// Database is the list of databases to migrate.
-	// +optional
-	Database []string `yaml:"database" json:"database,omitempty"`
-	// ExcludeDatabase is the list of databases to exclude from migration.
-	// +optional
-	ExcludeDatabase []string `yaml:"excludeDatabase" json:"excludeDatabase,omitempty"`
-}
-
-type MariaDBSnapshot struct {
-	// Enabled controls whether the Snapshot Phase should be executed.
-	// +optional
-	Enabled  bool                     `yaml:"enabled" json:"enabled"`
-	Pipeline *MariaDBSnapshotPipeline `yaml:"pipeline" json:"pipeline,omitempty"`
-}
-
-type MariaDBStreaming struct {
-	// Enabled controls whether the Logical Replication Phase should be executed.
-	// +optional
-	Enabled bool `yaml:"enabled" json:"enabled"`
-}
-
-type MariaDBConnectionInfo struct {
-	Address        string `yaml:"address" json:"address"`
-	User           string `yaml:"user" json:"user"`
-	Password       string `yaml:"password" json:"password"`
-	DBName         string `yaml:"dbName" json:"dbName"`
-	MaxConnections int    `yaml:"maxConnections" json:"maxConnections,omitempty"`
-}
-
-type MariaDBSnapshotPipeline struct {
-	Workers        *int `yaml:"workers" json:"workers"`
-	Sinkers        *int `yaml:"sinkers" json:"sinkers"`
-	Buffer         *int `yaml:"buffer" json:"buffer"`
-	ReadBatchSize  *int `yaml:"readBatchSize" json:"read_batch_size"`
-	WriteBatchSize *int `yaml:"writeBatchSize" json:"write_batch_size"`
-}
