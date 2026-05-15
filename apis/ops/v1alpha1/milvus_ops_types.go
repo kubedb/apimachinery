fc33a351639946ead6abac3c67bd1bdafe8b2db9
Tamal Saha <tamal@appscode.com>
2026-05-15 23:42:48 +0600

    Add StorageMigration to Milvus ops API
    Adds MilvusMigrationSpec, Migration field on MilvusOpsRequestSpec,
    and StorageMigration enum entry.
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/ops/v1alpha1/milvus_ops_types.go
+++ apis/ops/v1alpha1/milvus_ops_types.go
@@ -73,6 +73,8 @@ type MilvusOpsRequestSpec struct {
 	Authentication *AuthSpec `json:"authentication,omitempty"`
 	// Specifies information necessary for restarting database
 	Restart *RestartSpec `json:"restart,omitempty"`
+	// Specifies information necessary for migrating storage of the database
+	Migration *MilvusMigrationSpec `json:"migration,omitempty"`
 	// Timeout for each step of the ops request in second. If a step doesn't finish within the specified timeout, the ops request will result in failure.
 	Timeout *metav1.Duration `json:"timeout,omitempty"`
 	// ApplyOption is to control the execution of OpsRequest depending on the database state.
@@ -82,8 +84,8 @@ type MilvusOpsRequestSpec struct {
 	MaxRetries int32 `json:"maxRetries,omitempty"`
 }
 
-// +kubebuilder:validation:Enum=UpdateVersion;HorizontalScaling;VerticalScaling;VolumeExpansion;Restart;Reconfigure;ReconfigureTLS;RotateAuth
-// ENUM(UpdateVersion, HorizontalScaling, VerticalScaling, VolumeExpansion, Restart, Reconfigure, ReconfigureTLS, RotateAuth)
+// +kubebuilder:validation:Enum=UpdateVersion;HorizontalScaling;VerticalScaling;VolumeExpansion;Restart;Reconfigure;ReconfigureTLS;RotateAuth;StorageMigration
+// ENUM(UpdateVersion, HorizontalScaling, VerticalScaling, VolumeExpansion, Restart, Reconfigure, ReconfigureTLS, RotateAuth, StorageMigration)
 type MilvusOpsRequestType string
 
 // MilvusUpdateVersionSpec contains the update version information of a milvus cluster
@@ -137,6 +139,12 @@ type MilvusVolumeExpansionSpec struct {
 	Node *resource.Quantity `json:"node,omitempty"`
 	// volume specification for stremingnode
 	StreamingNode *resource.Quantity `json:"streamingnode,omitempty"`
+}
+
+type MilvusMigrationSpec struct {
+	StorageClassName *string `json:"storageClassName"`
+	// +optional
+	OldPVReclaimPolicy core.PersistentVolumeReclaimPolicy `json:"oldPVReclaimPolicy,omitempty"`
 }
 
 type MilvusTLSSpec struct {
