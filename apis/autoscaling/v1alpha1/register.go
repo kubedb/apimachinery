ee0f55a476b4911212ee01fcb8803d7511fcf877
Tamal Saha <tamal@appscode.com>
2026-05-18 09:20:45 +0600

    Add MilvusAutoscaler API types
    Register MilvusAutoscaler/MilvusAutoscalerList in the autoscaling/v1alpha1
    scheme so the autoscaler controller can manage compute + storage scaling
    for Milvus Standalone and Distributed topologies.

--- apis/autoscaling/v1alpha1/register.go
+++ apis/autoscaling/v1alpha1/register.go
@@ -76,6 +76,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&MariaDBAutoscalerList{},
 		&MemcachedAutoscaler{},
 		&MemcachedAutoscalerList{},
+		&MilvusAutoscaler{},
+		&MilvusAutoscalerList{},
 		&MongoDBAutoscaler{},
 		&MongoDBAutoscalerList{},
 		&MSSQLServerAutoscaler{},
