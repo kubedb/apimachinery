5c7947e8eba645d270a69948b50c06507edf33c7
Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>
2026-05-19 15:34:50 +0600

    Add Qdrant Autoscaler (#1648)
    Signed-off-by: Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>

--- apis/autoscaling/v1alpha1/register.go
+++ apis/autoscaling/v1alpha1/register.go
@@ -98,6 +98,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&PostgresAutoscalerList{},
 		&ProxySQLAutoscaler{},
 		&ProxySQLAutoscalerList{},
+		&QdrantAutoscaler{},
+		&QdrantAutoscalerList{},
 		&RabbitMQAutoscaler{},
 		&RabbitMQAutoscalerList{},
 		&RedisAutoscaler{},
--- client/informers/externalversions/generic.go
+++ client/informers/externalversions/generic.go
@@ -21,8 +21,6 @@ package externalversions
 import (
 	"fmt"
 
-	schema "k8s.io/apimachinery/pkg/runtime/schema"
-	cache "k8s.io/client-go/tools/cache"
 	v1alpha1 "kubedb.dev/apimachinery/apis/archiver/v1alpha1"
 	autoscalingv1alpha1 "kubedb.dev/apimachinery/apis/autoscaling/v1alpha1"
 	catalogv1alpha1 "kubedb.dev/apimachinery/apis/catalog/v1alpha1"
@@ -36,6 +34,9 @@ import (
 	postgresv1alpha1 "kubedb.dev/apimachinery/apis/postgres/v1alpha1"
 	schemav1alpha1 "kubedb.dev/apimachinery/apis/schema/v1alpha1"
 	uiv1alpha1 "kubedb.dev/apimachinery/apis/ui/v1alpha1"
+
+	schema "k8s.io/apimachinery/pkg/runtime/schema"
+	cache "k8s.io/client-go/tools/cache"
 )
 
 // GenericInformer is type of SharedIndexInformer which will locate and delegate to other
@@ -119,6 +120,8 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1alpha1().PostgresAutoscalers().Informer()}, nil
 	case autoscalingv1alpha1.SchemeGroupVersion.WithResource("proxysqlautoscalers"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1alpha1().ProxySQLAutoscalers().Informer()}, nil
+	case autoscalingv1alpha1.SchemeGroupVersion.WithResource("qdrantautoscalers"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1alpha1().QdrantAutoscalers().Informer()}, nil
 	case autoscalingv1alpha1.SchemeGroupVersion.WithResource("rabbitmqautoscalers"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Autoscaling().V1alpha1().RabbitMQAutoscalers().Informer()}, nil
 	case autoscalingv1alpha1.SchemeGroupVersion.WithResource("redisautoscalers"):
