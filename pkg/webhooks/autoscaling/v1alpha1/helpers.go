852e2b76dbbdd74ba74a8c21b98361043ecdc996
Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>
2026-05-19 10:21:15 +0600

    remove unintended change
    Signed-off-by: Abdullah Al Masood <98589987+al-masood@users.noreply.github.com>

--- pkg/webhooks/autoscaling/v1alpha1/helpers.go
+++ pkg/webhooks/autoscaling/v1alpha1/helpers.go
@@ -18,21 +18,105 @@ package v1alpha1
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 
 	autoscalingapi "kubedb.dev/apimachinery/apis/autoscaling/v1alpha1"
 	dbapi "kubedb.dev/apimachinery/apis/kubedb/v1"
 
+	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
+	"k8s.io/utils/ptr"
 )
 
 func setDefaultStorageValues(storageSpec *autoscalingapi.StorageAutoscalerSpec) {
-	storageSpec.SetDefaults()
+	if storageSpec == nil {
+		return
+	}
+	if storageSpec.Trigger == "" {
+		storageSpec.Trigger = autoscalingapi.AutoscalerTriggerOff
+	}
+	if storageSpec.UsageThreshold == nil {
+		storageSpec.UsageThreshold = ptr.To[int32](autoscalingapi.DefaultStorageUsageThreshold)
+	}
+	if storageSpec.ScalingThreshold == nil {
+		storageSpec.ScalingThreshold = ptr.To[int32](autoscalingapi.DefaultStorageScalingThreshold)
+	}
+	setDefaultScalingRules(storageSpec)
+}
+
+type quantity struct {
+	InString   string
+	InQuantity resource.Quantity
+	Threshold  string
+}
+
+// sort them by the appliesUpto value. The items with empty threshold
+func setDefaultScalingRules(storageSpec *autoscalingapi.StorageAutoscalerSpec) {
+	if storageSpec.ScalingRules == nil {
+		storageSpec.ScalingRules = []autoscalingapi.StorageScalingRule{
+			{
+				AppliesUpto: "",
+				Threshold:   fmt.Sprintf("%spc", strconv.Itoa(int(*storageSpec.ScalingThreshold))),
+			},
+		}
+	}
+	var quantities []quantity
+	var zeroQuantityThresholds []string
+	for _, sr := range storageSpec.ScalingRules {
+		if sr.AppliesUpto == "" {
+			zeroQuantityThresholds = append(zeroQuantityThresholds, sr.Threshold)
+			continue
+		}
+		quantities = append(quantities, quantity{
+			InString:   sr.AppliesUpto,
+			InQuantity: resource.MustParse(sr.AppliesUpto),
+			Threshold:  sr.Threshold,
+		})
+	}
+	sort.Slice(quantities, func(i, j int) bool {
+		return quantities[i].InQuantity.Cmp(quantities[j].InQuantity) < 0
+	})
+
+	storageSpec.ScalingRules = make([]autoscalingapi.StorageScalingRule, 0)
+	for _, q := range quantities {
+		storageSpec.ScalingRules = append(storageSpec.ScalingRules, autoscalingapi.StorageScalingRule{
+			AppliesUpto: q.InString,
+			Threshold:   q.Threshold,
+		})
+	}
+	for _, threshold := range zeroQuantityThresholds {
+		storageSpec.ScalingRules = append(storageSpec.ScalingRules, autoscalingapi.StorageScalingRule{
+			AppliesUpto: "",
+			Threshold:   threshold,
+		})
+	}
+	klog.Infof("scaling Rules = %v \n", storageSpec.ScalingRules)
 }
 
 func setDefaultComputeValues(computeSpec *autoscalingapi.ComputeAutoscalerSpec) {
-	computeSpec.SetDefaults()
+	if computeSpec == nil {
+		return
+	}
+	if computeSpec.Trigger == "" {
+		computeSpec.Trigger = autoscalingapi.AutoscalerTriggerOff
+	}
+	if computeSpec.ControlledResources == nil {
+		computeSpec.ControlledResources = []core.ResourceName{core.ResourceCPU, core.ResourceMemory}
+	}
+	if computeSpec.ContainerControlledValues == nil {
+		reqAndLim := autoscalingapi.ContainerControlledValuesRequestsAndLimits
+		computeSpec.ContainerControlledValues = &reqAndLim
+	}
+	if computeSpec.ResourceDiffPercentage == 0 {
+		computeSpec.ResourceDiffPercentage = autoscalingapi.DefaultResourceDiffPercentage
+	}
+	if computeSpec.PodLifeTimeThreshold.Duration == 0 {
+		computeSpec.PodLifeTimeThreshold = metav1.Duration{Duration: autoscalingapi.DefaultPodLifeTimeThreshold}
+	}
 }
 
 func setInMemoryDefaults(computeSpec *autoscalingapi.ComputeAutoscalerSpec, storageEngine dbapi.StorageEngine) {
