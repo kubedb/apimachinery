c103198ee16f1ffbfebe08cc395f881375fc9c07
Tamal Saha <tamal@appscode.com>
2026-06-04 13:33:06 +0600

    Update go.bytebuilders.dev/audit to v0.0.51 (#1744)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ vendor/kmodules.xyz/resource-metadata/apis/ui/v1alpha1/clusterprofile_types.go
@@ -0,0 +1,66 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	ResourceKindClusterProfile = "ClusterProfile"
+	ResourceClusterProfile     = "clusterprofile"
+	ResourceClusterProfiles    = "clusterprofiles"
+)
+
+// ClusterProfileSpec defines the desired state of ClusterProfile
+type ClusterProfileSpec struct {
+	Title               string              `json:"title"`
+	Description         string              `json:"description"`
+	Provider            string              `json:"provider,omitempty"`
+	RequiredFeatureSets map[string]Features `json:"requiredFeatureSets,omitempty"`
+}
+
+type Features []string
+
+// ClusterProfile is the Schema for the clusterprofiles API
+
+// +genclient
+// +genclient:nonNamespaced
+// +genclient:skipVerbs=updateStatus
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
+type ClusterProfile struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ClusterProfileSpec `json:"spec,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+
+// ClusterProfileList contains a list of ClusterProfile
+type ClusterProfileList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterProfile `json:"items"`
+}
