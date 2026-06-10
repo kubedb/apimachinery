d90741a1a7eac626796739bf1e9358a30ad7e8ce
Fazle Rabbi Sarker <73359077+fr-sarker@users.noreply.github.com>
2026-06-10 17:52:55 +0600

    update autoscaler api for missing database (#1752)
    Co-authored-by: souravbiswassanto <saurov@appscode.com>
    Co-authored-by: Urmi Biswas <139246745+biswasurmi@users.noreply.github.com>
    Signed-off-by: Fazle Rabbi Sarker <fazlerabbi@appscode.com>
    Signed-off-by: souravbiswassanto <saurov@appscode.com>
    Signed-off-by: urmi <urmi@appscode.com>

--- pkg/webhooks/autoscaling/v1alpha1/oracle.go
+++ pkg/webhooks/autoscaling/v1alpha1/oracle.go
@@ -89,12 +89,10 @@ func (w *OracleAutoscalerCustomWebhook) setDefaults(scaler *autoscalingapi.Oracl
 
 func (w *OracleAutoscalerCustomWebhook) setOpsReqOptsDefaults(scaler *autoscalingapi.OracleAutoscaler) {
 	if scaler.Spec.OpsRequestOptions == nil {
-		scaler.Spec.OpsRequestOptions = &autoscalingapi.OracleOpsRequestOptions{}
-	}
-	// Timeout is defaulted to 600s in ops-manager retries.go (to retry 120 times with 5sec pause between each)
-	// OplogMaxLagSeconds & ObjectsCountDiffPercentage are defaults to 0
-	if scaler.Spec.OpsRequestOptions.Apply == "" {
-		scaler.Spec.OpsRequestOptions.Apply = opsapi.ApplyOptionIfReady
+		scaler.Spec.OpsRequestOptions = &autoscalingapi.OpsRequestOptions{
+			Apply:      opsapi.ApplyOptionIfReady,
+			MaxRetries: 1,
+		}
 	}
 }
 
