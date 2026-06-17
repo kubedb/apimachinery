ce51e7e1ddcd3fa5334666bb9990059eacbb3c80
Tamal Saha <tamal@appscode.com>
2026-06-17 11:23:57 +0600

    Add gitops Ignite type (#1740)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/gitops/v1alpha1/openapi_generated.go
+++ apis/gitops/v1alpha1/openapi_generated.go
@@ -587,6 +587,9 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.ElasticsearchStatus":                           schema_apimachinery_apis_gitops_v1alpha1_ElasticsearchStatus(ref),
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.GitOpsInfo":                                    schema_apimachinery_apis_gitops_v1alpha1_GitOpsInfo(ref),
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.GitOpsStatus":                                  schema_apimachinery_apis_gitops_v1alpha1_GitOpsStatus(ref),
+		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.Ignite":                                        schema_apimachinery_apis_gitops_v1alpha1_Ignite(ref),
+		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.IgniteList":                                    schema_apimachinery_apis_gitops_v1alpha1_IgniteList(ref),
+		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.IgniteStatus":                                  schema_apimachinery_apis_gitops_v1alpha1_IgniteStatus(ref),
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.Kafka":                                         schema_apimachinery_apis_gitops_v1alpha1_Kafka(ref),
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.KafkaList":                                     schema_apimachinery_apis_gitops_v1alpha1_KafkaList(ref),
 		"kubedb.dev/apimachinery/apis/gitops/v1alpha1.KafkaStatus":                                   schema_apimachinery_apis_gitops_v1alpha1_KafkaStatus(ref),
@@ -33857,6 +33860,148 @@ func schema_apimachinery_apis_gitops_v1alpha1_GitOpsStatus(ref common.ReferenceC
 		},
 		Dependencies: []string{
 			"kubedb.dev/apimachinery/apis/gitops/v1alpha1.GitOpsInfo"},
+	}
+}
+
+func schema_apimachinery_apis_gitops_v1alpha1_Ignite(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type: []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta"),
+						},
+					},
+					"spec": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("kubedb.dev/apimachinery/apis/kubedb/v1alpha2.IgniteSpec"),
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("kubedb.dev/apimachinery/apis/gitops/v1alpha1.IgniteStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/apis/meta/v1.ObjectMeta", "kubedb.dev/apimachinery/apis/gitops/v1alpha1.IgniteStatus", "kubedb.dev/apimachinery/apis/kubedb/v1alpha2.IgniteSpec"},
+	}
+}
+
+func schema_apimachinery_apis_gitops_v1alpha1_IgniteList(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type: []string{"object"},
+				Properties: map[string]spec.Schema{
+					"kind": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"apiVersion": {
+						SchemaProps: spec.SchemaProps{
+							Description: "APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"metadata": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("k8s.io/apimachinery/pkg/apis/meta/v1.ListMeta"),
+						},
+					},
+					"items": {
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("kubedb.dev/apimachinery/apis/gitops/v1alpha1.Ignite"),
+									},
+								},
+							},
+						},
+					},
+				},
+				Required: []string{"items"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/apis/meta/v1.ListMeta", "kubedb.dev/apimachinery/apis/gitops/v1alpha1.Ignite"},
+	}
+}
+
+func schema_apimachinery_apis_gitops_v1alpha1_IgniteStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Type: []string{"object"},
+				Properties: map[string]spec.Schema{
+					"phase": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Specifies the current phase of the database",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"observedGeneration": {
+						SchemaProps: spec.SchemaProps{
+							Description: "observedGeneration is the most recent generation observed for this resource. It corresponds to the resource's generation, which is updated on mutation by the API Server.",
+							Type:        []string{"integer"},
+							Format:      "int64",
+						},
+					},
+					"conditions": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Conditions applied to the database, such as approval or denial.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Default: map[string]interface{}{},
+										Ref:     ref("kmodules.xyz/client-go/api/v1.Condition"),
+									},
+								},
+							},
+						},
+					},
+					"gitops": {
+						SchemaProps: spec.SchemaProps{
+							Default: map[string]interface{}{},
+							Ref:     ref("kubedb.dev/apimachinery/apis/gitops/v1alpha1.GitOpsStatus"),
+						},
+					},
+				},
+			},
+		},
+		Dependencies: []string{
+			"kmodules.xyz/client-go/api/v1.Condition", "kubedb.dev/apimachinery/apis/gitops/v1alpha1.GitOpsStatus"},
 	}
 }
 
