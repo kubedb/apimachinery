c8a929a7bcc98717d53b156c703e424eb68b17cf
Shuvo Kumar <shuvo@appscode.com>
2026-06-18 13:06:09 +0600

    Update ClickHouse Ops-Req status for status.PausedBackups
    Signed-off-by: Shuvo Kumar <shuvo@appscode.com>

--- client/clientset/versioned/typed/ops/v1alpha1/clickhouseopsrequest.go
+++ client/clientset/versioned/typed/ops/v1alpha1/clickhouseopsrequest.go
@@ -20,15 +20,21 @@ package v1alpha1
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	api "kubedb.dev/apimachinery/apis/ops/v1alpha1"
 	v1alpha1 "kubedb.dev/apimachinery/apis/ops/v1alpha1"
 	scheme "kubedb.dev/apimachinery/client/clientset/versioned/scheme"
 
+	kerr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	types "k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
 	watch "k8s.io/apimachinery/pkg/watch"
 	rest "k8s.io/client-go/rest"
+	kutil "kmodules.xyz/client-go"
 )
 
 // ClickHouseOpsRequestsGetter has a method to return a ClickHouseOpsRequestInterface.
@@ -192,5 +198,57 @@ func (c *clickHouseOpsRequests) Patch(ctx context.Context, name string, pt types
 		Body(data).
 		Do(ctx).
 		Into(result)
+	return
+}
+
+func UpdateClickHouseOpsRequestStatus(
+	ctx context.Context,
+	c OpsV1alpha1Interface,
+	meta metav1.ObjectMeta,
+	transform func(*api.OpsRequestStatus) (types.UID, *api.OpsRequestStatus),
+	opts metav1.UpdateOptions,
+) (result *api.ClickHouseOpsRequest, err error) {
+	apply := func(x *api.ClickHouseOpsRequest) *api.ClickHouseOpsRequest {
+		uid, updatedStatus := transform(x.Status.DeepCopy())
+		// Ignore status update when uid does not match
+		if uid != "" && uid != x.UID {
+			return x
+		}
+		return &api.ClickHouseOpsRequest{
+			TypeMeta:   x.TypeMeta,
+			ObjectMeta: x.ObjectMeta,
+			Spec:       x.Spec,
+			Status:     *updatedStatus,
+		}
+	}
+
+	attempt := 0
+	cur, err := c.ClickHouseOpsRequests(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	err = wait.PollUntilContextTimeout(ctx, kutil.RetryInterval, kutil.RetryTimeout, true, func(ctx context.Context) (bool, error) {
+		attempt++
+		var e2 error
+		result, e2 = c.ClickHouseOpsRequests(meta.Namespace).UpdateStatus(ctx, apply(cur), opts)
+		if kerr.IsConflict(e2) {
+			latest, e3 := c.ClickHouseOpsRequests(meta.Namespace).Get(ctx, meta.Name, metav1.GetOptions{})
+			switch {
+			case e3 == nil:
+				cur = latest
+				return false, nil
+			case kutil.IsRequestRetryable(e3):
+				return false, nil
+			default:
+				return false, e3
+			}
+		} else if err != nil && !kutil.IsRequestRetryable(e2) {
+			return false, e2
+		}
+		return e2 == nil, nil
+	})
+	if err != nil {
+		err = fmt.Errorf("failed to update status of ClickHouseOpsRequest %s/%s after %d attempts due to %v", meta.Namespace, meta.Name, attempt, err)
+	}
 	return
 }
