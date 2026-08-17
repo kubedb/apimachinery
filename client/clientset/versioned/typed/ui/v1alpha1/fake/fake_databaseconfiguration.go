03f54b82a8a698928a2c147102b9c2f555c70274
SK Ali Arman <arman@appscode.com>
2026-08-17 13:32:13 +0600

    make gen
    Signed-off-by: SK Ali Arman <arman@appscode.com>

--- client/clientset/versioned/typed/ui/v1alpha1/fake/fake_databaseconfiguration.go
+++ client/clientset/versioned/typed/ui/v1alpha1/fake/fake_databaseconfiguration.go
@@ -21,12 +21,11 @@ package fake
 import (
 	"context"
 
-	v1alpha1 "kubedb.dev/apimachinery/apis/ui/v1alpha1"
-
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
 	types "k8s.io/apimachinery/pkg/types"
 	testing "k8s.io/client-go/testing"
+	v1alpha1 "kubedb.dev/apimachinery/apis/ui/v1alpha1"
 )
 
 // FakeDatabaseConfigurations implements DatabaseConfigurationInterface
